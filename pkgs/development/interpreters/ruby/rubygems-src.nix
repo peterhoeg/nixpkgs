2c701fda23b2c127694fd888d73db528ac40a58f
Peter Hoeg <peter@speartail.com>
2016-08-26 17:20:31 +0800

    rubygems: downgrade to 2.6.1 due to errors with 2.6.2

--- pkgs/development/interpreters/ruby/rubygems-src.nix
+++ pkgs/development/interpreters/ruby/rubygems-src.nix
@@ -1,6 +1,6 @@
 { fetchurl
-, version ? "2.6.2"
-, sha256 ? "1j02ajici555f35vd6ky6m4bxs8lh8nqb1c59qqib4jp4ibcv6zy"
+, version ? "2.6.1"
+, sha256 ? "0kqzmrd91h19q5y84wyx53rx0x09hdffp84gasy0a73s6sld3i69"
 }:
 fetchurl {
   url = "http://production.cf.rubygems.org/rubygems/rubygems-${version}.tgz";
