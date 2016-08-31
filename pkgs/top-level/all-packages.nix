0e70f5e4a13b5fd4f981ede49fa0f0d129305c90
Peter Hoeg <peter@speartail.com>
2016-08-31 11:26:23 +0800

    ledger-web: init at 1.5.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13778,6 +13778,7 @@ in
     boost = boost155;
   };
   ledger = self.ledger3;
+  ledger-web = callPackage ../applications/office/ledger-web { };
 
   lighthouse = callPackage ../applications/misc/lighthouse { };
 
