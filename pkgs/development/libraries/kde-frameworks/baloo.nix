57e491ebe9ea8e1c986f99013657836fcd7e318e
Peter Hoeg <peter@speartail.com>
2016-10-17 17:54:50 +0800

    trigger dbus services via systemd

--- pkgs/development/libraries/kde-frameworks/baloo.nix
+++ pkgs/development/libraries/kde-frameworks/baloo.nix
@@ -1,9 +1,36 @@
 { kdeFramework, lib, ecm, kauth, kconfig
 , kcoreaddons, kcrash, kdbusaddons, kfilemetadata, ki18n, kidletime
-, kio, lmdb, qtbase, solid
+, kio, lmdb, qtbase, solid, writeText
 }:
 
-kdeFramework {
+let
+  busName = "org.kde.baloo";
+  serviceName = "baloo.service";
+
+  dbus = writeText "dbus-${serviceName}" ''
+    [D-BUS Service]
+    Name=${busName}
+    Exec=@out@/bin/baloo_file
+    SystemdService=dbus-${busName}.service
+  '';
+
+  service = writeText serviceName ''
+    [Unit]
+    Description=Baloo File Indexer
+
+    [Service]
+    Type=dbus
+    BusName=${busName}
+    ExecStart=@out@/bin/baloo_file
+    ExecStop=@out@/bin/balooctl stop
+    ExecReload=@out@/bin/balooctl reload
+    Restart=on-failure
+    PrivateTmp=true
+    Slice=kde.slice
+    Nice=10
+  '';
+
+in kdeFramework {
   name = "baloo";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
   nativeBuildInputs = [ ecm ];
@@ -11,4 +38,14 @@ kdeFramework {
     kauth kconfig kcoreaddons kcrash kdbusaddons kfilemetadata ki18n kio
     kidletime lmdb qtbase solid
   ];
+
+  postInstall = ''
+    mkdir -p $out/lib/systemd/user $out/share/dbus-1/services
+    sed "s|@out@|$out|g" ${service} > $out/lib/systemd/user/${serviceName}
+    ln -sr $out/lib/systemd/user/${serviceName} $out/lib/systemd/user/dbus-${busName}.service
+
+    rm -f $out/etc/xdg/autostart/${serviceName}
+
+    sed "s|@out@|$out|g" ${dbus} > $out/share/dbus-1/services/${busName}.service
+  '';
 }
--- pkgs/development/libraries/kde-frameworks/kwallet.nix
+++ pkgs/development/libraries/kde-frameworks/kwallet.nix
@@ -1,9 +1,35 @@
 { kdeFramework, lib, ecm, kconfig, kconfigwidgets
 , kcoreaddons , kdbusaddons, kdoctools, ki18n, kiconthemes
 , knotifications , kservice, kwidgetsaddons, kwindowsystem, libgcrypt
+, writeText
 }:
 
-kdeFramework {
+let
+  busName = "org.kde.kwalletd5";
+  serviceName = "kwalletd5.service";
+
+  dbus = writeText "dbus-${serviceName}" ''
+    [D-BUS Service]
+    Name=${busName}
+    Exec=@out@/bin/kwalletd5
+    SystemdService=dbus-${busName}.service
+  '';
+
+  service = writeText serviceName ''
+    [Unit]
+    Description=KDE Wallet
+
+    [Service]
+    Type=dbus
+    BusName=${busName}
+    ExecStart=@out@/bin/kwalletd5
+    ExecStop=${kdbusaddons}/bin/kquitapp5 kwalletd5
+    Restart=on-failure
+    PrivateTmp=true
+    Slice=kde.slice
+  '';
+
+in kdeFramework {
   name = "kwallet";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
   nativeBuildInputs = [ ecm kdoctools ];
@@ -11,4 +37,14 @@ kdeFramework {
     kconfig kconfigwidgets kcoreaddons kdbusaddons ki18n kiconthemes
     knotifications kservice kwidgetsaddons kwindowsystem libgcrypt
   ];
+
+  postInstall = ''
+    mkdir -p $out/lib/systemd/user
+    sed "s|@out@|$out|g" ${service} > $out/lib/systemd/user/${serviceName}
+    ln -sr $out/lib/systemd/user/${serviceName} $out/lib/systemd/user/dbus-${busName}.service
+
+    rm -f $out/etc/xdg/autostart/${serviceName}
+
+    sed "s|@out@|$out|g" ${dbus} > $out/share/dbus-1/services/${busName}.service
+  '';
 }
