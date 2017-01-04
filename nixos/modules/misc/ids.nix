be13083dd8a5322c4f021e828a32a392b8c4a021
Peter Hoeg <peter@speartail.com>
2017-01-04 17:05:07 +0800

    nixos module: sensu

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -284,6 +284,8 @@
 
       sensu = 300;
 
+      sensu = 300;
+
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
       nixbld = 30000; # start of range of uids
@@ -534,6 +536,8 @@
       riak-cs = 263;
       infinoted = 264;
       keystone = 265;
+
+      sensu = 300;
 
       sensu = 300;
 
