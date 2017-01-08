5016a9640cd441bf699eabf54144f973b3dba6cc
Peter Hoeg <peter@speartail.com>
2017-01-08 23:17:34 +0800

    user ids

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -283,6 +283,9 @@
       keystone = 265;
       glance = 266;
 
+      sensu = 300;
+      kodi = 301;
+
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
       nixbld = 30000; # start of range of uids
@@ -535,6 +538,8 @@
       keystone = 265;
       glance = 266;
 
+      sensu = 300;
+      kodi = 301;
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
       # uids and gids. Also, don't use gids above 399!
