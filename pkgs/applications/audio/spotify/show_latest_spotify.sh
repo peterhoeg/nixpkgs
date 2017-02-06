7a9186e888e7602eb9112176c0bbd3e25384b971
Peter Hoeg <peter@speartail.com>
2017-02-06 13:46:35 +0800

    version helper

---
+++ pkgs/applications/audio/spotify/show_latest_spotify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p curl gzip gnugrep coreutils
+
+set -euo pipefail
+IFS=$'\n\t'
+
+URL=http://repository.spotify.com/dists/stable/non-free/binary-amd64/Packages.gz 
+
+curl --silent $URL | gunzip | grep Version | sort -u | tail -n1 | cut -f3 -d ':'
