d6cc2cd00d485bee4278d487a9c60a7ef3bb2772
Peter Hoeg <peter@speartail.com>
2017-02-08 10:41:25 +0800

    gcalcli: fix notifications on linux

--- pkgs/applications/misc/gcalcli/default.nix
+++ pkgs/applications/misc/gcalcli/default.nix
@@ -1,13 +1,14 @@
-{ fetchFromGitHub, lib, pythonPackages }:
+{ stdenv, lib, fetchFromGitHub, pythonPackages
+, libnotify ? null }:
 
 pythonPackages.buildPythonApplication rec {
   version = "3.4.0";
   name = "gcalcli-${version}";
 
   src = fetchFromGitHub {
-    owner = "insanum";
-    repo = "gcalcli";
-    rev = "v${version}";
+    owner  = "insanum";
+    repo   = "gcalcli";
+    rev    = "v${version}";
     sha256 = "171awccgnmfv4j7m2my9387sjy60g18kzgvscl6pzdid9fn9rrm8";
   };
 
@@ -20,12 +21,15 @@ pythonPackages.buildPythonApplication rec {
     parsedatetime
     six
     vobject
-  ] ++ lib.optional (!pythonPackages.isPy3k) futures;
+  ]
+  ++ lib.optional (!pythonPackages.isPy3k) futures
+  ++ lib.optional stdenv.isLinux libnotify;
 
   meta = with lib; {
     homepage = https://github.com/insanum/gcalcli;
     description = "CLI for Google Calendar";
     license = licenses.mit;
-    maintainers = [ maintainers.nocoolnametom ];
+    maintainers = with maintainers; [ nocoolnametom ];
+    inherit version;
   };
 }
