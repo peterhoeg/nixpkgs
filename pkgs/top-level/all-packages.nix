90114adbc6f9c9ae4fbb33e6b719e9eeaaa5943f
Nick Hu <me@nickhu.co.uk>
2017-02-13 23:39:29 +0000

    fdr: init at 4.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13290,6 +13290,8 @@ with pkgs;
 
   fbreader = callPackage ../applications/misc/fbreader { };
 
+  fdr = qt57.callPackage ../applications/science/programming/fdr { };
+
   fehlstart = callPackage ../applications/misc/fehlstart { };
 
   fetchmail = callPackage ../applications/misc/fetchmail { };
