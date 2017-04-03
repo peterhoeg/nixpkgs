a5bb634a5ff496dd8e06e723d85923c0fae47fcb
Peter Hoeg <peter@hoeg.com>
2017-04-03 09:34:54 +0800

    python wrapping is broken

--- pkgs/applications/networking/sync/backintime/common.nix
+++ pkgs/applications/networking/sync/backintime/common.nix
@@ -1,46 +1,60 @@
-{stdenv, fetchFromGitHub, makeWrapper, gettext, python3Packages, rsync, cron, openssh, sshfs-fuse, encfs }:
+{ stdenv, fetchFromGitHub, makeWrapper, which
+, gettext, python3Packages, rsync, cron, openssh, sshfs-fuse, encfs }:
 
-let
-  inherit (python3Packages) python dbus-python keyring;
-in stdenv.mkDerivation rec {
-  version = "1.1.12";
-
+python3Packages.buildPythonApplication rec {
   name = "backintime-common-${version}";
+  version = "1.1.18";
+
+  # TODO: version 1.2.0 has been ported to qt5
 
   src = fetchFromGitHub {
-    owner = "bit-team";
-    repo = "backintime";
-    rev = "v${version}";
-    sha256 = "0n3x48wa8aa7i8fff85h3b5h3xpabk51ld0ymy3pkqh0krfgs59a";
+    owner  = "bit-team";
+    repo   = "backintime";
+    rev    = "v${version}";
+    sha256 = "0djkqgbb33r5rb1w99kgk61m2c3ccvnnw3pdafmyw6s9rpd126r1";
   };
 
-  buildInputs = [ makeWrapper gettext python dbus-python keyring openssh cron rsync sshfs-fuse encfs ];
+  buildInputs = [ gettext openssh cron rsync sshfs-fuse encfs ];
+  nativeBuildInputs = [ makeWrapper which ];
+  propagatedBuildInputs = with python3Packages; [
+    dbus-python keyring
+  ];
 
-  installFlags = [ "DEST=$(out)" ];
+  dontConfigure = true;
+  dontInstall = true;
+  doCheck = false;
 
-  preConfigure = "cd common";
+  buildPhase = ''
+    cd common
+    substituteInPlace configure --replace 'PREFIX=/usr' 'PREFIX=$out'
+    ./configure
+    make
+    make install
+  '';
 
-  dontAddPrefix = true;
-
-  preFixup =
-    ''
+  preFixup = ''
     substituteInPlace "$out/bin/backintime" \
       --replace "=\"/usr/share" "=\"$prefix/share"
+
+    patchPythonScript $out/share/backintime/common/*.py
+
+    substituteInPlace $out/bin/backintime \
+      --replace 'python3 ' '${python3Packages.python.interpreter} '
+
     wrapProgram "$out/bin/backintime" \
-      --prefix PYTHONPATH : "$PYTHONPATH" \
-      --prefix PATH : "$prefix/bin:$PATH"
+      --prefix PYTHONPATH : "$PYTHONPATH"
     '';
 
-  meta = {
-    homepage = https://github.com/bit-team/backintime;
+  meta = with stdenv.lib; {
     description = "Simple backup tool for Linux";
-    license = stdenv.lib.licenses.gpl2;
-    maintainers = [ ];
-    platforms = stdenv.lib.platforms.all;
     longDescription = ''
       Back In Time is a simple backup tool (on top of rsync) for Linux
       inspired from “flyback project” and “TimeVault”. The backup is
       done by taking snapshots of a specified set of directories.
     '';
+    homepage    = https://github.com/bit-team/backintime;
+    license     = licenses.gpl2;
+    maintainers = [ peterhoeg ];
+    platforms   = platforms.all;
   };
 }
