e6bac597feb104f1f6ca2a6b0fdcd36f4deeb76e
Peter Hoeg <peter@hoeg.com>
2017-05-12 15:24:18 +0800

    powerdevil: add missing X deps

--- pkgs/desktops/plasma-5/powerdevil.nix
+++ pkgs/desktops/plasma-5/powerdevil.nix
@@ -1,7 +1,7 @@
 { plasmaPackage, extra-cmake-modules, kdoctools, bluez-qt, kactivities
 , kauth, kconfig, kdbusaddons, kdelibs4support, kglobalaccel, ki18n
 , kidletime, kio, knotifyconfig, kwayland, libkscreen, networkmanager-qt
-, plasma-workspace, qtx11extras, solid, udev
+, plasma-workspace, qtx11extras, solid, udev, libpthreadstubs, libXdmcp
 }:
 
 plasmaPackage {
@@ -11,5 +11,6 @@ plasmaPackage {
     kconfig kdbusaddons knotifyconfig solid udev bluez-qt kactivities kauth
     kdelibs4support kglobalaccel ki18n kio kidletime kwayland libkscreen
     networkmanager-qt plasma-workspace qtx11extras
+    libpthreadstubs libXdmcp
   ];
 }
