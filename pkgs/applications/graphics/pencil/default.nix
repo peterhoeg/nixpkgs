12341e20bd2486123f33162eb45c7541d5286ba1
Peter Hoeg <peter@hoeg.com>
2017-07-20 10:03:40 +0800

    pencil wip

--- pkgs/applications/graphics/pencil/default.nix
+++ pkgs/applications/graphics/pencil/default.nix
@@ -1,34 +1,80 @@
-{ stdenv, fetchurl, makeWrapper, xulrunner }:
+{ stdenv, fetchurl, dpkg, makeWrapper
+, alsaLib, atk, cairo, cups, dbus, expat, fontconfig, freetype, gdk_pixbuf, glib, gnome2, gtk2, nspr, nss, pango, udev
+, libX11, libXcursor, libXcomposite, libXdamage, libXext, libXfixes
+, libXi, libXrandr, libXrender, libXScrnSaver, libXtst }:
 
 stdenv.mkDerivation rec {
-  version = "2.0.21";
+  version = "3.0.4";
   name = "pencil-${version}";
 
   src = fetchurl {
-    url = "https://github.com/prikhi/pencil/releases/download/v${version}/Pencil-${version}-linux-pkg.tar.gz";
-    sha256 = "0xq3gczqy7gzf1997qxdql5z7qqk1vabr0rzgakmsi4dq2q4d3kq";
+    url = "http://pencil.evolus.vn/dl/V${version}/Pencil_${version}_amd64.deb";
+    sha256 = "1zb2g99nv0sr9b1nx0jrwdqp0zy835z1fkrpv0aqgshmqsabgqjq";
   };
 
-  buildPhase = "";
+  unpackPhase = "dpkg-deb -x $src .";
+  dontBuild = true;
 
-  buildInputs = [ makeWrapper ];
+  nativeBuildInputs = [ dpkg makeWrapper ];
+
+  libPath = stdenv.lib.makeLibraryPath [
+    alsaLib
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk_pixbuf
+    glib
+    gnome2.GConf
+    gtk2
+    nspr
+    nss
+    pango
+    stdenv.cc.cc
+    udev
+    libX11 libXcursor libXcomposite libXdamage libXext libXfixes
+    libXi libXrandr libXrender libXScrnSaver libXtst
+  ];
 
   installPhase = ''
-    mkdir -p "$out"
-    cp -r usr/* "$out"
-    sed -e "s|/usr/share/evolus-pencil|$out/share/evolus-pencil|" \
-        -i "$out/bin/pencil"
-    sed -e "s|/usr/bin/pencil|$out/bin/pencil|" \
-        -e "s|Icon=.*|Icon=$out/share/evolus-pencil/skin/classic/icon.svg|" \
-        -i "$out/share/applications/pencil.desktop"
+    runHook preInstall
 
-    wrapProgram $out/bin/pencil \
-      --prefix PATH ":" ${xulrunner}/bin
+    mkdir -p $out/bin
+    cp -r usr/* $out
+    cp -r opt   $out
+
+    ln -s $out/opt/Pencil/pencil $out/bin/pencil
+
+    # sed -e "s|/usr/share/evolus-pencil|$out/share/evolus-pencil|" \
+    #     -i "$out/bin/pencil"
+    # sed -e "s|/usr/bin/pencil|$out/bin/pencil|" \
+    #     -e "s|Icon=.*|Icon=$out/share/evolus-pencil/skin/classic/icon.svg|" \
+    #     -i "$out/share/applications/pencil.desktop"
+
+  #   wrapProgram $out/bin/pencil \
+  #     --prefix PATH ":" {xulrunner}/bin
+  '';
+
+  preFixup = ''
+    patchelf \
+      --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
+      --set-rpath "${libPath}:$out/opt/Pencil" \
+      $out/opt/Pencil/pencil
+
+    for f in $out/opt/Pencil/lib*.so ; do
+      patchelf \
+        --set-rpath "${stdenv.lib.makeLibraryPath [ stdenv.cc.cc ]}:$out/opt/Pencil" \
+        $f
+      chmod 644 $f
+    done
   '';
 
   meta = with stdenv.lib; {
     description = "GUI prototyping/mockup tool";
-    homepage = http://github.com/prikhi/pencil;
+    homepage = https://pencil.evolus.vn/;
     license = licenses.gpl2; # Commercial license is also available
     maintainers = with maintainers; [ bjornfor prikhi ];
     platforms = platforms.linux;
