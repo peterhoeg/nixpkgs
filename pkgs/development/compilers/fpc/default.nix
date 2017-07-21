2d0ce1c6e713fcf2d87b7577ad0447d8441665fc
Peter Hoeg <peter@hoeg.com>
2017-07-21 12:34:43 +0800

    fpc: 3.0.0 -> 3.0.2
    Additionally:
    
     - patch fpc.cfg with a path to glibc to fix linking errors
     - compile a one-liner at the end to ensure that the compiler actually works

--- pkgs/development/compilers/fpc/default.nix
+++ pkgs/development/compilers/fpc/default.nix
@@ -1,44 +1,71 @@
-{ stdenv, fetchurl, gawk }:
+{ stdenv, fetchurl, binutils, gawk, makeWrapper }:
 
-let startFPC = import ./binary.nix { inherit stdenv fetchurl; }; in
+let
+  startFPC = import ./binary.nix { inherit stdenv fetchurl; };
 
-stdenv.mkDerivation rec {
-  version = "3.0.0";
+in stdenv.mkDerivation rec {
+  version = "3.0.2";
   name = "fpc-${version}";
 
   src = fetchurl {
     url = "mirror://sourceforge/freepascal/fpcbuild-${version}.tar.gz";
-    sha256 = "1v40bjp0kvsi8y0mndqvvhnsqjfssl2w6wpfww51j4rxblfkp4fm";
+    sha256 = "0x8vajb3brmh078hxz0pydym1wbxf1dxca7lzxlh268z6q5fsqgj";
   };
 
-  buildInputs = [ startFPC gawk ];
+  nativeBuildInputs = [ gawk makeWrapper startFPC ];
 
-  preConfigure =
-    if stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux" then ''
-      sed -e "s@'/lib/ld-linux[^']*'@'''@" -i fpcsrc/compiler/systems/t_linux.pas
-      sed -e "s@'/lib64/ld-linux[^']*'@'''@" -i fpcsrc/compiler/systems/t_linux.pas
-    '' else "";
+  preConfigure = stdenv.lib.optionalString stdenv.isLinux ''
+    substituteInPlace fpcsrc/compiler/systems/t_linux.pas \
+      --replace "'/lib/ld-linux"   "'${stdenv.cc.libc}/lib/ld-linux" \
+      --replace "'/lib64/ld-linux" "'${stdenv.cc.libc}/lib/ld-linux"
+  '';
 
-  makeFlags = "NOGDB=1 FPC=${startFPC}/bin/fpc";
+  makeFlags = [
+    "FPC=${startFPC}/bin/fpc"
+    "NOGDB=1"
+    "OPT=-Fl${stdenv.cc.libc}/lib"
+    "RELEASE=1"
+    "build"
+  ];
 
-  installFlags = "INSTALL_PREFIX=\${out}";
-  
+  installFlags = [
+    "INSTALL_PREFIX=\${out}"
+  ];
+
   postInstall = ''
     for i in $out/lib/fpc/*/ppc*; do
       ln -fs $i $out/bin/$(basename $i)
     done
     mkdir -p $out/lib/fpc/etc/
     $out/lib/fpc/*/samplecfg $out/lib/fpc/${version} $out/lib/fpc/etc/
+    cat <<_EOF >>$out/lib/fpc/etc/fpc.cfg
+# added by nixpkgs:
+-Fl${stdenv.glibc.out}/lib
+_EOF
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/fpc \
+      --prefix PATH : "$out/bin:${stdenv.lib.makeBinPath [ binutils ]}"
+  '';
+
+
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    echo "program Hello; begin writeln ('Hello, world!') end." > hello.pas
+    $out/bin/fpc hello.pas
+    ./hello
   '';
 
   passthru = {
     bootstrap = startFPC;
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Free Pascal Compiler from a source distribution";
-    maintainers = [stdenv.lib.maintainers.raskin];
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = with maintainers; [ raskin ];
+    inherit (startFPC.meta) platforms;
     inherit version;
   };
 }
