fec54ed2bc45ca7c1d45d577db9d6f19c039b2cd
Peter Hoeg <peter@hoeg.com>
2017-07-23 16:31:31 +0800

    thefuck wip

--- pkgs/tools/misc/thefuck/default.nix
+++ pkgs/tools/misc/thefuck/default.nix
@@ -1,25 +1,40 @@
-{ fetchurl, stdenv, pkgs, ... }:
+{ stdenv, fetchFromGitHub, python3Packages }:
 
-pkgs.pythonPackages.buildPythonPackage rec {
-  name = "${pname}-${version}";
-  pname = "thefuck";
+python3Packages.buildPythonApplication rec {
+  name = "thefuck-${version}";
   version = "3.18";
 
-  src = fetchurl {
-    url = "https://github.com/nvbn/${pname}/archive/${version}.tar.gz";
-    sha256 = "1xsvkqh89rgxq5w03mnlcfkn9y39nfwhb2pjabjspcc2mi2mq5y6";
+  src = fetchFromGitHub {
+    owner  = "nvbn";
+    repo   = "thefuck";
+    rev    = version;
+    sha256 = "14fg5faip6s4j9c7799bkswc6j5hb5npcg8nwrjjfdgr05jp7389";
   };
 
-  propagatedBuildInputs = with pkgs.pythonPackages; [
-    psutil
-    colorama
-    six
-    decorator
-    pathlib2
+  propagatedBuildInputs = with python3Packages; [
+    colorama decorator pathlib2 psutil six
   ];
 
+  checkInputs = with python3Packages; [ mock pytest pytest-mock tox ];
+
+  # prePatch = ''
+  #   for f in thefuck/shells/*.py ; do
+  #     substituteInPlace $f --replace thefuck $out/bin/thefuck
+  #   done
+
+  #   for f in tests/shells/test_*.py ; do
+  #     substituteInPlace $f --replace '$(thefuck' "\$($out/bin/thefuck"
+  #   done
+  # '';
+
+  checkPhase = ''
+    export PYTHONIOENCODING=utf8
+    export LANG="en_US.UTF-8"
+    py.test --capture=sys
+  '';
+
   meta = with stdenv.lib; {
-    homepage = "https://github.com/nvbn/thefuck";
+    homepage = https://github.com/nvbn/thefuck;
     description = "Magnificent app which corrects your previous console command.";
     license = licenses.mit;
     maintainers = with maintainers; [ ma27 ];
