a887fbbff8db2b50d468bf2c784572d68dec3e37
Peter Hoeg <peter@hoeg.com>
2017-08-17 13:47:49 +0800

    wip

--- pkgs/servers/amqp/rabbitmq-server/default.nix
+++ pkgs/servers/amqp/rabbitmq-server/default.nix
@@ -1,40 +1,88 @@
-{ stdenv, fetchurl, erlang, python, libxml2, libxslt, xmlto
+{ stdenv, fetchFromGitHub, perl, erlang, python, libxml2, libxslt, xmlto
 , docbook_xml_dtd_45, docbook_xsl, zip, unzip, rsync
-
 , AppKit, Carbon, Cocoa
 }:
 
-stdenv.mkDerivation rec {
-  name = "rabbitmq-server-${version}";
-
+let
   version = "3.6.6";
 
-  src = fetchurl {
-    url = "https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_6_6/rabbitmq-server-3.6.6.tar.xz";
-    sha256 = "13mpnyfxd026w525rsnkcw0f8bcrkbzl7k9g8pnqmm3zyny8jmir";
+  common = stdenv.mkDerivation rec {
+    name = "rabbitmq-common-${version}";
+    src = fetchFromGitHub {
+      owner  = "rabbitmq";
+      repo   = "rabbitmq-common";
+      rev    = stdenv.lib.replaceStrings [ "." ] [ "_" ]"rabbitmq_v${version}";
+      sha256 = "1qdbnvq38kpkz2wq1qfwvnp6a6ldi5xcd9s124dyndpfxhh3nh4l";
+    };
+    buildInputs = [ erlang ];
+    installPhase = ''
+      mkdir -p $out
+      mv ebin $out/
+    '';
   };
 
-  buildInputs =
-    [ erlang python libxml2 libxslt xmlto docbook_xml_dtd_45 docbook_xsl zip unzip rsync ]
-    ++ stdenv.lib.optionals stdenv.isDarwin [ AppKit Carbon Cocoa ];
-
-  preBuild =
-    ''
-      # Fix the "/usr/bin/env" in "calculate-relative".
-      patchShebangs .
+  ranch = stdenv.mkDerivation rec {
+    name = "ranch-${version}";
+    version = "0.6.1";
+    src = fetchFromGitHub {
+      owner  = "rabbitmq";
+      repo   = "ranch";
+      rev    = version;
+      sha256 = "06m9jpjr79vp3q861asc1i6b5in8rcyrgjw0azv6fgnqh8w12kpi";
+    };
+    buildInputs = [ erlang ];
+    installPhase = ''
+      mkdir -p $out
+      mv ebin $out/
     '';
+  };
+
+in stdenv.mkDerivation rec {
+  name = "rabbitmq-server-${version}";
+
+  src = fetchFromGitHub {
+    owner  = "rabbitmq";
+    repo   = "rabbitmq-server";
+    rev    = stdenv.lib.replaceStrings [ "." ] [ "_" ]"rabbitmq_v${version}";
+    sha256 = "1h3lrar7svajx7ccf6zrn8csr2hlxc8fwnvpx62pndvjw9nzgdm7";
+  };
+
+  buildInputs = [
+    erlang libxml2 libxslt zip unzip rsync
+    common ranch
+  ] ++ stdenv.lib.optionals stdenv.isDarwin [ AppKit Carbon Cocoa ];
+  nativeBuildInputs = [
+    docbook_xml_dtd_45 docbook_xsl xmlto
+    perl python
+  ];
+
+  preBuild = ''
+    # Fix the "/usr/bin/env" in "calculate-relative".
+    patchShebangs .
+
+    export CLI_ESCRIPTS_DIR=$out/bin
+    # export DEPS_DIR=$out/deps
+
+    mkdir -p deps
+
+    ln -s ${common}   deps/rabbit_common
+    ln -s ${ranch}    deps/ranch
+  '';
 
   installFlags = "PREFIX=$(out) RMQ_ERLAPP_DIR=$(out)";
+
   installTargets = "install install-man";
 
-  postInstall =
-    ''
-      echo 'PATH=${erlang}/bin:''${PATH:+:}$PATH' >> $out/sbin/rabbitmq-env
-    ''; # */
+  postInstall = ''
+    mkdir -p $out/share/doc/rabbitmq-server
+    mv $out/LICEN* $out/share/doc/rabbitmq-server
 
-  meta = {
-    homepage = http://www.rabbitmq.com/;
+    echo 'PATH=${erlang}/bin:''${PATH:+:}$PATH' >> $out/sbin/rabbitmq-env
+  '';
+
+  meta = with stdenv.lib; {
     description = "An implementation of the AMQP messaging protocol";
-    platforms = stdenv.lib.platforms.unix;
+    homepage = http://www.rabbitmq.com/;
+    platforms = platforms.unix;
   };
 }
