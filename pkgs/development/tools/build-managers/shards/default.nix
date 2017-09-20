916f112858fe2730b0e72becea6e0bddaebfd663
Peter Hoeg <peter@hoeg.com>
2017-09-20 14:56:59 +0800

    shards: cleanup

--- pkgs/development/tools/build-managers/shards/default.nix
+++ pkgs/development/tools/build-managers/shards/default.nix
@@ -1,28 +1,28 @@
-{ stdenv, fetchurl, crystal, libyaml, which }:
+{ stdenv, fetchFromGitHub, crystal, libyaml, which }:
 
 stdenv.mkDerivation rec {
   name = "shards-${version}";
   version = "0.7.1";
 
-  src = fetchurl {
-    url = "https://github.com/crystal-lang/shards/archive/v${version}.tar.gz";
-    sha256 = "31de819c66518479682ec781a39ef42c157a1a8e6e865544194534e2567cb110";
+  src = fetchFromGitHub {
+    owner  = "crystal-lang";
+    repo   = "shards";
+    rev    = "v${version}";
+    sha256 = "09rama4xpm9xvs63nqxlhl84gppm6i750rq4m4khp208ln4jmphd";
   };
 
-  buildInputs = [ crystal libyaml which ];
+  buildInputs = [ crystal libyaml ];
+
+  nativeBuildInputs = [ which ];
 
   buildFlags = [ "CRFLAGS=" "release" ];
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp bin/shards $out/bin/
+    install -Dm755 bin/shards $out/bin/shards
   '';
 
   meta = with stdenv.lib; {
-    homepage = https://crystal-lang.org/;
-    license = licenses.asl20;
     description = "Dependency manager for the Crystal language";
-    maintainers = with maintainers; [ mingchuan ];
-    platforms = [ "x86_64-linux" "i686-linux" "x86_64-darwin" ];
+    inherit (crystal.meta) homepage license maintainers platforms;
   };
 }
