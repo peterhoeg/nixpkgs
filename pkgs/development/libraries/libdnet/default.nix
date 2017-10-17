cc3f6aa0da213e62458814733dce226075943706
Peter Hoeg <peter@hoeg.com>
2017-10-17 18:32:41 +0800

    libdnet: new upstream

--- pkgs/development/libraries/libdnet/default.nix
+++ pkgs/development/libraries/libdnet/default.nix
@@ -1,16 +1,16 @@
-{stdenv, fetchurl, automake, autoconf, libtool}:
+{ stdenv, fetchFromGitHub, autoreconfHook }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "libdnet-1.12";
 
-  enableParallelBuilding = true;
-
-  src = fetchurl {
-    url = http://libdnet.googlecode.com/files/libdnet-1.12.tgz;
-    sha1 = "71302be302e84fc19b559e811951b5d600d976f8";
+  src = fetchFromGitHub {
+    owner  = "dugsong";
+    repo   = "libdnet";
+    rev    = name;
+    sha256 = "1g2x0g1irxlhg2m3nd5d1vycmp489a7nafnnyalsh1w90fzlfw05";
   };
 
-  buildInputs = [ automake autoconf libtool ];
+  enableParallelBuilding = true;
 
   # .so endings are missing (quick and dirty fix)
   postInstall = ''
@@ -19,11 +19,11 @@ stdenv.mkDerivation {
     done
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Provides a simplified, portable interface to several low-level networking routines";
-    homepage = http://code.google.com/p/libdnet/;
-    license = stdenv.lib.licenses.bsd3;
-    maintainers = [stdenv.lib.maintainers.marcweber];
-    platforms = stdenv.lib.platforms.linux;
+    homepage    = https://github.com/dugsong/libdnet;
+    license     = licenses.bsd3;
+    maintainers = with maintainers; [ marcweber ];
+    platforms   = platforms.linux;
   };
 }
