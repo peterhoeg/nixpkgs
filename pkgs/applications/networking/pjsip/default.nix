8c4e6efdf970b2e35f60b152065529fc168ca3d1
Peter Hoeg <peter@hoeg.com>
2017-10-25 10:45:00 +0800

    pjsip: 2.6 -> 2.7 wip

--- pkgs/applications/networking/pjsip/default.nix
+++ pkgs/applications/networking/pjsip/default.nix
@@ -1,31 +1,52 @@
-{ stdenv, fetchurl, openssl, libsamplerate, alsaLib }:
+{ stdenv, fetchurl, autoreconfHook, pkgconfig
+, automake, autoconf
+, alsaLib, openssl, libopus, libsamplerate, portaudio, speex, srtp }:
 
 stdenv.mkDerivation rec {
   name = "pjsip-${version}";
-  version = "2.6";
+  version = "2.7";
 
   src = fetchurl {
-    url = "http://www.pjsip.org/release/${version}/pjproject-${version}.tar.bz2";
-    sha256 = "1d67c58jn22f7h6smkykk5vwl3sqpc7xi2vm3j3lbn3lq6hisnig";
+    url    = "http://www.pjsip.org/release/${version}/pjproject-${version}.tar.bz2";
+    sha256 = "0swgn821nc5m4fcrwbxv2b3iy0zgr2mhc7siqarm34f324d6500w";
   };
 
-  buildInputs = [ openssl libsamplerate alsaLib ];
+  hardeningDisable = [ "pic" ];
+
+  postPatch = ''
+    mv aconfigure    configure
+    mv aconfigure.ac configure.ac
+  '';
+
+  configureFlags = [
+    "--with-external-pa"
+    "--with-external-speex"
+    "--with-external-srtp"
+  ];
+
+  buildInputs = [ alsaLib openssl libopus libsamplerate portaudio speex srtp ];
+
+  nativeBuildInputs = [ autoconf automake pkgconfig ];
+
+  enableParallelBuilding = true;
 
   postInstall = ''
-    mkdir -p $out/bin
-    cp pjsip-apps/bin/pjsua-* $out/bin/pjsua
-    mkdir -p $out/share/${name}/samples
+    mkdir -p $out/{bin,share/${name}/samples}
+
+    cp pjsip-apps/bin/pjsua-*     $out/bin/pjsua
     cp pjsip-apps/bin/samples/*/* $out/share/${name}/samples
   '';
+
+  doCheck = true;
 
   # We need the libgcc_s.so.1 loadable (for pthread_cancel to work)
   dontPatchELF = true;
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A multimedia communication library written in C, implementing standard based protocols such as SIP, SDP, RTP, STUN, TURN, and ICE";
-    homepage = http://pjsip.org/;
-    license = stdenv.lib.licenses.gpl2Plus;
-    maintainers = with stdenv.lib.maintainers; [viric];
-    platforms = with stdenv.lib.platforms; linux;
+    homepage    = https://pjsip.org/;
+    license     = stdenv.lib.licenses.gpl2Plus;
+    maintainers = with maintainers; [ viric ];
+    platforms   = with platforms; linux;
   };
 }
