71ea20dbe53870a9d1d80e7e3238a0724dacdbce
Peter Hoeg <peter@hoeg.com>
2017-12-29 12:24:42 +0800

    wip

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16456,7 +16456,7 @@ with pkgs;
 
   mirage = callPackage ../applications/graphics/mirage { };
 
-  mixxx = callPackage ../applications/audio/mixxx {
+  mixxx = libsForQt5.callPackage ../applications/audio/mixxx {
     inherit (vamp) vampSDK;
   };
 
