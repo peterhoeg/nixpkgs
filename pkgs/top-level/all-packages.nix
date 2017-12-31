7d5db61b7a339307281be3735f3edd122b912c9d
Peter Hoeg <peter@hoeg.com>
2018-01-01 01:40:54 +0800

    sensu-check-wmiplus: 1.63

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13489,6 +13489,8 @@ with pkgs;
 
   sensu = callPackage ../servers/monitoring/sensu { };
 
+  sensu-check-wmiplus = callPackage ../servers/monitoring/sensu/checks/wmiplus.nix { };
+
   uchiwa = callPackage ../servers/monitoring/uchiwa { };
 
   shishi = callPackage ../servers/shishi {
