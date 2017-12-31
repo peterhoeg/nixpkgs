dac2604a0474f88c690a6eeae6778ae710783262
Peter Hoeg <peter@hoeg.com>
2018-01-01 01:41:21 +0800

    perlPackages.ConfigIniFiles: init at 2.94

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -2360,6 +2360,21 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
+  ConfigIniFiles = buildPerlPackage rec {
+    name = "Config-IniFiles-2.94";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/S/SH/SHLOMIF/${name}.tar.gz";
+      sha256 = "1d4la72fpsf61hcpslmn03ajm5rfy8hm50piqmsfi7d7dm0qmlyn";
+    };
+    propagatedBuildInputs = [ IOStringy ModuleBuild ];
+    meta = with stdenv.lib; {
+      homepage = https://github.com/shlomif/perl-Config-IniFiles;
+      description = "A module for reading .ini-style configuration files";
+      license = with licenses; [ artistic1 gpl1Plus ];
+      maintainers = with maintainers; [ peterhoeg ];
+    };
+  };
+
   ConfigMerge = buildPerlPackage {
     name = "Config-Merge-1.04";
     src = fetchurl {
