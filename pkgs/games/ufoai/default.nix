1896055873ed1ce98cc4d338eb438bfe03d1fdd7
Peter Hoeg <peter@hoeg.com>
2018-01-21 10:59:37 +0800

    ufoai wip

--- pkgs/games/ufoai/default.nix
+++ pkgs/games/ufoai/default.nix
@@ -1,36 +1,47 @@
 { stdenv, fetchurl, libtheora, xvidcore, mesa, SDL, SDL_ttf, SDL_mixer
-, curl, libjpeg, libpng, gettext, cunit, enableEditor?false }:
+, curl, libjpeg, libpng, gettext, cunit
+, openal, zip, python
+, enableEditor ? false }:
 
-stdenv.mkDerivation rec {
-  name = "ufoai-2.4";
-  src = fetchurl {
-    url = "mirror://sourceforge/ufoai/${name}-source.tar.bz2";
-    sha256 = "0kxrbcjrharcwz319s90m789i4my9285ihp5ax6kfhgif2vn2ji5";
-  };
+let
+  name = "ufoai-2.5";
 
   srcData = fetchurl {
     url = "mirror://sourceforge/ufoai/${name}-data.tar";
-    sha256 = "1drhh08cqqkwv1yz3z4ngkplr23pqqrdx6cp8c3isy320gy25cvb";
+    sha256 = "1c6dglmm36qj522q1pzahxrqjkihsqlq2yac1aijwspz9916lw2y";
   };
 
-  preConfigure = ''tar xvf "${srcData}"'';
+in stdenv.mkDerivation rec {
+  inherit name;
+
+  src = fetchurl {
+    url = "mirror://sourceforge/ufoai/${name}-source.tar.bz2";
+    sha256 = "13ra337mjc56kw31m7z77lc8vybngkzyhvmy3kvpdcpyksyc6z0c";
+  };
+
+  preConfigure = ''
+    tar xvf "${srcData}"
+  '';
 
   configureFlags = [ "--enable-release" "--enable-sse" ]
     ++ stdenv.lib.optional enableEditor "--enable-uforadiant";
 
   buildInputs = [
     libtheora xvidcore mesa SDL SDL_ttf SDL_mixer
-    curl libjpeg libpng gettext cunit
+    curl libjpeg libpng
+    openal
+    zip python
   ];
+
+  nativeBuildInputs = [ cunit gettext ];
 
   NIX_CFLAGS_LINK = "-lgcc_s"; # to avoid occasional runtime error in finding libgcc_s.so.1
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = http://ufoai.org;
     description = "A squad-based tactical strategy game in the tradition of X-Com";
-    license = stdenv.lib.licenses.gpl2Plus;
-    maintainers = with stdenv.lib.maintainers; [viric];
-    platforms = stdenv.lib.platforms.linux;
-    hydraPlatforms = [];
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [viric];
+    platforms = platforms.linux;
   };
 }
