635a5d4ed67c97042b7f94e4fc189a82cc3caf65
Peter Hoeg <peter@hoeg.com>
2018-02-13 23:59:19 +0800

    fpc

--- pkgs/development/compilers/fpc/default.nix
+++ pkgs/development/compilers/fpc/default.nix
@@ -1,28 +1,31 @@
 { stdenv, fetchurl, gawk }:
 
-let startFPC = import ./binary.nix { inherit stdenv fetchurl; }; in
+let
+  startFPC = import ./binary.nix { inherit stdenv fetchurl; };
 
-stdenv.mkDerivation rec {
-  version = "3.0.0";
+in stdenv.mkDerivation rec {
   name = "fpc-${version}";
+  version = "3.0.4";
 
   src = fetchurl {
     url = "mirror://sourceforge/freepascal/fpcbuild-${version}.tar.gz";
-    sha256 = "1v40bjp0kvsi8y0mndqvvhnsqjfssl2w6wpfww51j4rxblfkp4fm";
+    sha256 = "0xjyhlhz846jbnp12y68c7nq4xmp4i65akfbrjyf3r62ybk18rgn";
   };
 
   buildInputs = [ startFPC gawk ];
 
   preConfigure =
     if stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux" then ''
-      sed -e "s@'/lib/ld-linux[^']*'@'''@" -i fpcsrc/compiler/systems/t_linux.pas
-      sed -e "s@'/lib64/ld-linux[^']*'@'''@" -i fpcsrc/compiler/systems/t_linux.pas
+      linker=$(gcc -print-file-name=ld-linux-x86-64.so.2)
+      sed -i fpcsrc/compiler/systems/t_linux.pas \
+        -e "s@'/lib/ld-linux[^']*'@'$linker'@" \
+        -e "s@'/lib64/ld-linux[^']*'@'$linker'@"
     '' else "";
 
   makeFlags = "NOGDB=1 FPC=${startFPC}/bin/fpc";
 
   installFlags = "INSTALL_PREFIX=\${out}";
-  
+
   postInstall = ''
     for i in $out/lib/fpc/*/ppc*; do
       ln -fs $i $out/bin/$(basename $i)
@@ -35,10 +38,10 @@ stdenv.mkDerivation rec {
     bootstrap = startFPC;
   };
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Free Pascal Compiler from a source distribution";
-    maintainers = [stdenv.lib.maintainers.raskin];
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.linux;
     inherit version;
   };
 }
