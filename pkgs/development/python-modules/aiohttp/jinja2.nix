1c9839e2c11f69d159802660c6cd32538718ba35
Peter Hoeg <peter@hoeg.com>
2018-02-28 10:01:19 +0800

    aiohttp-jinja2: init at 0.16.0

---
+++ pkgs/development/python-modules/aiohttp/jinja2.nix
@@ -0,0 +1,20 @@
+{ lib, stdenv, buildPythonPackage, fetchPypi, aiohttp, jinja2 }:
+
+buildPythonPackage rec {
+  pname = "aiohttp-jinja2";
+  version = "0.16.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1ps182yrc5g9ph55927a7ssqx6m9kx0bivfxpaj8sa3znrdkl94d";
+  };
+
+  propagatedBuildInputs = [ aiohttp jinja2 ];
+
+  meta = with lib; {
+    description = "Jinja2 support for aiohttp";
+    homepage = https://github.com/aio-libs/aiohttp_jinja2;
+    license = licenses.asl20;
+    maintainers = with maintainers; [ peterhoeg ];
+  };
+}
