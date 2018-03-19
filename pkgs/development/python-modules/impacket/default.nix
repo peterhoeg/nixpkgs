acc2c69b7fac2f292a3278befe949dc11d13a9c4
Peter Hoeg <peter@hoeg.com>
2018-03-19 18:49:45 +0800

    pythonPackages.impacket: init at 0.9.15

---
+++ pkgs/development/python-modules/impacket/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, buildPythonPackage, fetchPypi }:
+
+buildPythonPackage rec {
+  pname = "impacket";
+  version = "0.9.15";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "1sq1698g7wqj731h24f7gr4lc0fz0mxrqv6mm3j4hm2j6h3rrbr6";
+  };
+
+  meta = with stdenv.lib; {
+    description = "Network protocols Constructors and Dissectors";
+    homepage = https://github.com/CoreSecurity/impacket;
+    # Modified Apache Software License, Version 1.1
+    license = licenses.free;
+    maintainers = with maintainers; [ peterhoeg ];
+  };
+}
