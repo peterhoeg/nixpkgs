5f94664b64e8cd03bc02dfdd87aaac73a600b0cf
Peter Hoeg <peter@hoeg.com>
2018-05-17 12:25:57 +0800

    syslinux wip

--- pkgs/os-specific/linux/syslinux/default.nix
+++ pkgs/os-specific/linux/syslinux/default.nix
@@ -1,37 +1,37 @@
 { stdenv, fetchFromGitHub, fetchurl, nasm, perl, python, libuuid, mtools, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  name = "syslinux-2015-11-09";
+  name = "syslinux-2017-11-23";
 
   src = fetchFromGitHub {
     owner = "geneC";
     repo = "syslinux";
-    rev = "0cc9a99e560a2f52bcf052fd85b1efae35ee812f";
-    sha256 = "0wk3r5ki4lc334f9jpml07wpl8d0bnxi9h1l4h4fyf9a0d7n4kmw";
+    rev = "2ea44cbedb297bd6b409d5c1e0402d5f89592be4";
+    sha256 = "1rb1gngr67r401w59zj1z2w850cwn4w0lb5pdxayz7kbvnx5bvv9";
   };
 
   patches = [
-    ./perl-deps.patch
-    (fetchurl {
-      # ldlinux.elf: Not enough room for program headers, try linking with -N
-      name = "not-enough-room.patch";
-      url = "https://anonscm.debian.org/cgit/collab-maint/syslinux.git/plain/"
-          + "debian/patches/0014_fix_ftbfs_no_dynamic_linker.patch?id=a556ad7";
-      sha256 = "0ijqjsjmnphmvsx0z6ppnajsfv6xh6crshy44i2a5klxw4nlvrsw";
-    })
+    # ./perl-deps.patch
+  #   (fetchurl {
+  #     # ldlinux.elf: Not enough room for program headers, try linking with -N
+  #     name = "not-enough-room.patch";
+  #     url = "https://anonscm.debian.org/cgit/collab-maint/syslinux.git/plain/"
+  #         + "debian/patches/0014_fix_ftbfs_no_dynamic_linker.patch?id=a556ad7";
+  #     sha256 = "0ijqjsjmnphmvsx0z6ppnajsfv6xh6crshy44i2a5klxw4nlvrsw";
+  #   })
   ];
 
   nativeBuildInputs = [ nasm perl python ];
   buildInputs = [ libuuid makeWrapper ];
 
-  enableParallelBuilding = false; # Fails very rarely with 'No rule to make target: ...'
+  enableParallelBuilding = true; # Fails very rarely with 'No rule to make target: ...'
   hardeningDisable = [ "pic" "stackprotector" "fortify" ];
 
   preBuild = ''
     substituteInPlace Makefile --replace /bin/pwd $(type -P pwd)
-    substituteInPlace gpxe/src/Makefile.housekeeping --replace /bin/echo $(type -P echo)
     substituteInPlace utils/ppmtolss16 --replace /usr/bin/perl $(type -P perl)
-    substituteInPlace gpxe/src/Makefile --replace /usr/bin/perl $(type -P perl)
+    # substituteInPlace gpxe/src/Makefile.housekeeping --replace /bin/echo $(type -P echo)
+    # substituteInPlace gpxe/src/Makefile --replace /usr/bin/perl $(type -P perl)
   '';
 
   stripDebugList = "bin sbin share/syslinux/com32";
