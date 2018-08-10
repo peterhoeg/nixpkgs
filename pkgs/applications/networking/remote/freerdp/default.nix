8a4ab04a8ef92d538c9090fbfd313665804685cf
Peter Hoeg <peter@hoeg.com>
2018-08-10 21:22:30 +0800

    freerdp: 2.0.0-rc2 -> 2.0.0-3c3

--- pkgs/applications/networking/remote/freerdp/default.nix
+++ pkgs/applications/networking/remote/freerdp/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   name = "freerdp-${version}";
-  version = "2.0.0-rc2";
+  version = "2.0.0-rc3";
 
   src = fetchFromGitHub {
     owner  = "FreeRDP";
     repo   = "FreeRDP";
     rev    = version;
-    sha256 = "01cm9g4xqihnnc5d2w1zs8gabkv59p7fyjwi1cwpzv6s198xwbfs";
+    sha256 = "0lpn6klwfnw69imgiibn3mff7lzli2idphgvw5lnradbfw4pr9qc";
   };
 
   # outputs = [ "bin" "out" "dev" ];
@@ -42,11 +42,7 @@ stdenv.mkDerivation rec {
     wayland
   ] ++ optional stdenv.isLinux systemd;
 
-  nativeBuildInputs = [
-    cmake pkgconfig
-  ];
-
-  enableParallelBuilding = true;
+  nativeBuildInputs = [ cmake pkgconfig ];
 
   doCheck = false;
 
