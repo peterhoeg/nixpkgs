29c90aeedbc652ae695594216c42f658488e2d47
Peter Hoeg <peter@hoeg.com>
2018-10-19 15:46:04 +0800

    pythonPackages.snapcast: init at 2.0.9

---
+++ pkgs/development/python-modules/snapcast/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, buildPythonPackage, fetchPypi, pytest
+, construct }:
+
+buildPythonPackage rec {
+  pname = "snapcast";
+  version = "2.0.9";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0hlfcg0qdfavjzhxz4v2spjkj6440a1254wrncfkfkyff6rv9x3s";
+  };
+
+  checkInputs = [ pytest ];
+
+  propagatedBuildInputs = [ construct ];
+
+  # no checks from Pypi - https://github.com/happyleavesaoc/python-snapcast/issues/23
+  doCheck = false;
+
+  meta = with stdenv.lib; {
+    description = "Control Snapcast, a multi-room synchronous audio solution";
+    homepage = https://github.com/happyleavesaoc/python-snapcast/;
+    license = licenses.mit;
+    maintainers = with maintainers; [ peterhoeg ];
+  };
+}
