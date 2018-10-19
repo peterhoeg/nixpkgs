3681d26482b50ad3013cf527cc21b2579073d6fe
Peter Hoeg <peter@hoeg.com>
2018-10-19 09:43:57 +0800

    spectral: 2018-09-24 -> 2018-10-18

--- pkgs/applications/networking/instant-messengers/spectral/default.nix
+++ pkgs/applications/networking/instant-messengers/spectral/default.nix
@@ -1,33 +1,51 @@
-{ stdenv, fetchgit
+{ stdenv, fetchFromGitLab
 , pkgconfig
 , qmake, qtbase, qtquickcontrols2, qtmultimedia
-, libpulseaudio
+, libpulseaudio, libqmatrixclient
 # Not mentioned but seems needed
 , qtgraphicaleffects
 # Unsure but needed by similar
 , qtdeclarative, qtsvg
 }:
 
-stdenv.mkDerivation rec {
-  name = "spectral-${version}";
-  version = "2018-09-24";
-
-  src = fetchgit {
-    url = "https://gitlab.com/b0/spectral.git";
-    rev = "c9d1d6887722860a52b597a0f74d0ce39c8622e1";
-    sha256 = "1ym8jlqls4lcq5rd81vxw1dni79fc6ph00ip8nsydl6i16fngl4c";
-    fetchSubmodules = true;
+let
+  sort-filter-proxy-model = fetchFromGitLab {
+    owner  = "b0";
+    repo   = "SortFilterProxyModel";
+    rev    = "c61f2bdb0da48804a596a9a3a9382eebdba764dc";
+    sha256 = "102dciv082nhaav9x06q7i3dani3rpaa0llciav92j178v6ldkkb";
   };
 
+in stdenv.mkDerivation rec {
+  name = "spectral-${version}";
+  version = "2018-10-18";
+
+  src = fetchFromGitLab {
+    owner  = "b0";
+    repo   = "spectral";
+    rev    = "d35405696ffeb92e370a5a9600c7a478f55cde78";
+    sha256 = "0xm8gkxqk75c0xs69dgr0nc241zcb7d9hcrwvvhc4dh3543y0mpn";
+  };
+
+  # libqmatrixclient is still not ready so we are using the source
+  postPatch = ''
+    rm -rf include/{SortFilterProxyModel,libqmatrixclient}
+    ln -sf ${sort-filter-proxy-model} include/SortFilterProxyModel
+    ln -sf ${libqmatrixclient.src}    include/libqmatrixclient
+  '';
+
   nativeBuildInputs = [ pkgconfig qmake ];
-  buildInputs = [ qtbase qtquickcontrols2 qtmultimedia qtgraphicaleffects qtdeclarative qtsvg ]
-    ++ stdenv.lib.optional stdenv.hostPlatform.isLinux libpulseaudio;
+  buildInputs = [
+    qtbase qtquickcontrols2 qtmultimedia qtgraphicaleffects qtdeclarative qtsvg
+  ] ++ stdenv.lib.optional stdenv.hostPlatform.isLinux libpulseaudio;
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "A glossy client for Matrix, written in QtQuick Controls 2 and C++";
     homepage = https://gitlab.com/b0/spectral;
     license = licenses.gpl3;
-    platforms = with platforms; linux ++ darwin;
     maintainers = with maintainers; [ dtzWill ];
+    platforms = with platforms; linux ++ darwin;
   };
 }
