19d114a13d2bcadc039223c4beb5cd06c2188cc2
Peter Hoeg <peter@hoeg.com>
2019-01-30 17:21:17 +0800

    nixos/lcd: implement RFC42

--- nixos/modules/services/hardware/lcd.nix
+++ nixos/modules/services/hardware/lcd.nix
@@ -1,25 +1,34 @@
 { config, lib, pkgs, ... }:
-
 let
+  inherit (lib)
+    mkEnableOption mkOption mkIf types
+    mkRemovedOptionModule;
+
   cfg = config.services.hardware.lcd;
   pkg = lib.getBin pkgs.lcdproc;
 
-  serverCfg = pkgs.writeText "lcdd.conf" ''
-    [server]
-    DriverPath=${pkg}/lib/lcdproc/
-    ReportToSyslog=false
-    Bind=${cfg.serverHost}
-    Port=${toString cfg.serverPort}
-    ${cfg.server.extraConfig}
-  '';
+  cfgFmt = pkgs.formats.ini { };
 
-  clientCfg = pkgs.writeText "lcdproc.conf" ''
-    [lcdproc]
-    Server=${cfg.serverHost}
-    Port=${toString cfg.serverPort}
-    ReportToSyslog=false
-    ${cfg.client.extraConfig}
-  '';
+  serverCfg = cfgFmt.generate "lcdd.conf" (lib.recursiveUpdate
+    {
+      server = {
+        DriverPath = "${pkg}/lib/lcdproc/";
+        Bind = cfg.serverHost;
+        Port = cfg.serverPort;
+        ReportToSyslog = false;
+      };
+    }
+    cfg.server.settings);
+
+  clientCfg = cfgFmt.generate "lcdproc.conf" (lib.recursiveUpdate
+    {
+      lcdproc = {
+        Server = cfg.serverHost;
+        Port = cfg.serverPort;
+        ReportToSyslog = false;
+      };
+    }
+    cfg.client.settings);
 
   serviceCfg = {
     DynamicUser = true;
@@ -27,104 +36,102 @@ let
     Slice = "lcd.slice";
   };
 
-in with lib; {
+in
+{
 
-  meta.maintainers = with maintainers; [ peterhoeg ];
+  meta.maintainers = with lib.maintainers; [ peterhoeg ];
+
+  imports = [
+    (mkRemovedOptionModule [ "services" "hardware" "lcd" "server" "extraConfig" ] "Use services.hardware.lcd.server.settings instead.")
+    (mkRemovedOptionModule [ "services" "hardware" "lcd" "client" "extraConfig" ] "Use services.hardware.lcd.client.settings instead.")
+  ];
 
   options = with types; {
     services.hardware.lcd = {
       serverHost = mkOption {
+        description = "Host on which LCDd is listening.";
         type = str;
         default = "localhost";
-        description = "Host on which LCDd is listening.";
       };
 
       serverPort = mkOption {
+        description = "Port on which LCDd is listening.";
         type = int;
         default = 13666;
-        description = "Port on which LCDd is listening.";
       };
 
       server = {
-        enable = mkOption {
-          type = bool;
-          default = false;
-          description = "Enable the LCD panel server (LCDd)";
-        };
+        enable = mkEnableOption "LCD panel server (LCDd)";
 
         openPorts = mkOption {
-          type = bool;
-          default = false;
           description = "Open the ports in the firewall";
-        };
-
-        usbPermissions = mkOption {
           type = bool;
           default = false;
-          description = ''
-            Set group-write permissions on a USB device.
-            </para>
-            <para>
-            A USB connected LCD panel will most likely require having its
-            permissions modified for lcdd to write to it. Enabling this option
-            sets group-write permissions on the device identified by
-            <option>services.hardware.lcd.usbVid</option> and
-            <option>services.hardware.lcd.usbPid</option>. In order to find the
-            values, you can run the <command>lsusb</command> command. Example
-            output:
-            </para>
-            <para>
-            <literal>
-            Bus 005 Device 002: ID 0403:c630 Future Technology Devices International, Ltd lcd2usb interface
-            </literal>
-            </para>
-            <para>
-            In this case the vendor id is 0403 and the product id is c630.
-          '';
         };
 
-        usbVid = mkOption {
-          type = str;
-          default = "";
-          description = "The vendor ID of the USB device to claim.";
+        usb = {
+          permissions = mkOption {
+            description = ''
+              Set group-write permissions on a USB device.
+              </para>
+              <para>
+              A USB connected LCD panel will most likely require having its
+              permissions modified for lcdd to write to it. Enabling this option
+              sets group-write permissions on the device identified by
+              <option>services.hardware.lcd.usbVid</option> and
+              <option>services.hardware.lcd.usbPid</option>. In order to find the
+              values, you can run the <command>lsusb</command> command. Example
+              output:
+              </para>
+              <para>
+              <literal>
+              Bus 005 Device 002: ID 0403:c630 Future Technology Devices International, Ltd lcd2usb interface
+              </literal>
+              </para>
+              <para>
+              In this case the vendor id is 0403 and the product id is c630.
+            '';
+            type = bool;
+            default = false;
+          };
+
+          vid = mkOption {
+            description = "The vendor ID of the USB device to claim.";
+            type = str;
+          };
+
+          pid = mkOption {
+            description = "The product ID of the USB device to claim.";
+            type = str;
+          };
+
+          group = mkOption {
+            description = "The group to grant write permissions on the USB device";
+            type = str;
+            default = "dialout";
+          };
         };
 
-        usbPid = mkOption {
-          type = str;
-          default = "";
-          description = "The product ID of the USB device to claim.";
-        };
-
-        usbGroup = mkOption {
-          type = str;
-          default = "dialout";
-          description = "The group to use for settings permissions. This group must exist or you will have to create it.";
-        };
-
-        extraConfig = mkOption {
-          type = lines;
-          default = "";
-          description = "Additional configuration added verbatim to the server config.";
+        settings = mkOption {
+          description = "Additional configuration added to the server config.";
+          type = cfgFmt.type;
+          default = { };
         };
       };
 
       client = {
-        enable = mkOption {
-          type = bool;
-          default = false;
-          description = "Enable the LCD panel client (LCDproc)";
-        };
-
-        extraConfig = mkOption {
-          type = lines;
-          default = "";
-          description = "Additional configuration added verbatim to the client config.";
-        };
+        enable = mkEnableOption "LCD panel client (LCDproc)";
 
         restartForever = mkOption {
+          description = "Try restarting the client forever.";
           type = bool;
           default = true;
-          description = "Try restarting the client forever.";
+        };
+
+        settings = mkOption {
+          description = "Additional configuration added to the client config.";
+          type = cfgFmt.type;
+          default = { };
         };
       };
     };
@@ -133,9 +140,17 @@ in with lib; {
   config = mkIf (cfg.server.enable || cfg.client.enable) {
     networking.firewall.allowedTCPPorts = mkIf (cfg.server.enable && cfg.server.openPorts) [ cfg.serverPort ];
 
-    services.udev.extraRules = mkIf (cfg.server.enable && cfg.server.usbPermissions) ''
-      ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="${cfg.server.usbVid}", ATTRS{idProduct}=="${cfg.server.usbPid}", MODE="660", GROUP="${cfg.server.usbGroup}"
-    '';
+    services.udev.extraRules = mkIf cfg.server.enable (lib.concatStringsSep ", " ([
+      ''ACTION=="add"''
+      ''SUBSYSTEMS=="usb"''
+      ''ATTRS{idVendor}=="${cfg.server.usb.vid}"''
+      ''ATTRS{idProduct}=="${cfg.server.usb.pid}"''
+      ''SYMLINK="lcd"''
+      ''TAG+="systemd"''
+    ] ++ lib.optionals cfg.server.usb.permissions [
+      ''MODE="0664"''
+      ''GROUP="${cfg.server.usb.group}"''
+    ]));
 
     systemd.services = {
       lcdd = mkIf cfg.server.enable {
@@ -143,7 +158,7 @@ in with lib; {
         wantedBy = [ "lcd.target" ];
         serviceConfig = serviceCfg // {
           ExecStart = "${pkg}/bin/LCDd -f -c ${serverCfg}";
-          SupplementaryGroups = cfg.server.usbGroup;
+          SupplementaryGroups = mkIf cfg.server.usb.permissions [ cfg.server.usb.group ];
         };
       };
 
