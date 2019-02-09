94899e37586f98e42074836cf323b6ae22de0af6
Peter Hoeg <peter@hoeg.com>
2019-02-09 12:37:47 +0800

    mosquitto: 1.5.5 -> 1.5.6

--- pkgs/servers/mqtt/mosquitto/default.nix
+++ pkgs/servers/mqtt/mosquitto/default.nix
@@ -1,30 +1,22 @@
-{ stdenv, fetchFromGitHub, fetchpatch, cmake, docbook_xsl, libxslt
+{ stdenv, fetchFromGitHub, fetchurl, cmake, docbook_xsl, libxslt
 , openssl, libuuid, libwebsockets, c-ares, libuv }:
 
 stdenv.mkDerivation rec {
   name = "mosquitto-${version}";
-  version = "1.5.5";
+  version = "1.5.6";
 
-  src = fetchFromGitHub {
-    owner  = "eclipse";
-    repo   = "mosquitto";
-    rev    = "v${version}";
-    sha256 = "1sfwmvrglfy5gqfk004kvbjldqr36dqz6xmppbgfhr47j5zs66xc";
+  src = fetchurl {
+    url = "https://mosquitto.org/embargo/78436548/${name}.tar.gz";
+    sha256 = "0h0sfph1azb3fxv70h38kw1fxahhvqagqmvd6wk00db8qqyc3gfm";
   };
 
-  postPatch = ''
-    substituteInPlace man/manpage.xsl \
-      --replace /usr/share/xml/docbook/stylesheet/ ${docbook_xsl}/share/xml/
-
-    for f in {lib,lib/cpp,src}/CMakeLists.txt ; do
-      substituteInPlace $f --replace /sbin/ldconfig ldconfig
-    done
-
-    # the manpages are not generated when using cmake
-    pushd man
-    make
-    popd
-  '';
+  # TODO: switch back to this when normal releases are available at GH again
+  # src = fetchFromGitHub {
+  #   owner  = "eclipse";
+  #   repo   = "mosquitto";
+  #   rev    = "v${version}";
+  #   sha256 = "1sfwmvrglfy5gqfk004kvbjldqr36dqz6xmppbgfhr47j5zs66xc";
+  # };
 
   buildInputs = [ openssl libuuid libwebsockets c-ares libuv ];
 
