fb8867de4f497e35836e077883c11439c3400daf
Peter Hoeg <peter@hoeg.com>
2019-02-19 12:34:31 +0800

    tora: 3.1 -> 3.2.176

--- pkgs/development/tools/tora/default.nix
+++ pkgs/development/tools/tora/default.nix
@@ -1,23 +1,30 @@
 { mkDerivation, lib, fetchFromGitHub, cmake, extra-cmake-modules, makeWrapper
-, boost, doxygen, openssl, mysql, postgresql, graphviz, loki, qscintilla, qtbase }:
+, boost, doxygen, openssl, mysql, postgresql, graphviz, loki, qscintilla, qtbase, qttools }:
 
 let
-  qscintillaLib = (qscintilla.override { withQt5 = true; });
+  # This and the check in preConfigure is to avoid going through the entire
+  # build before failing when trying to link against a non-existent library file
+  # as we cannot use cmake/modules/FindQScintilla.cmake which looks in qtbase
+  # and for the wrong library name
+  qsctLibFile = "${qscintilla}/lib/libqscintilla2_qt5.so";
 
 in mkDerivation rec {
   name = "tora-${version}";
-  version = "3.1";
+  version = "3.2.176";
 
   src = fetchFromGitHub {
     owner  = "tora-tool";
     repo   = "tora";
-    rev    = "v${version}";
-    sha256 = "0wninl10bcgiljf6wnhn2rv8kmzryw78x5qvbw8s2zfjlnxjsbn7";
+    # Upstream doesn't tag point releases, so we have to find the commit of the release manually
+    # https://github.com/tora-tool/tora/issues/125
+    rev    = "39bf2837779bf458fc72a9f0e49271152e57829f";
+    sha256 = "0fr9b542i8r6shgnz33lc3cz333fnxgmac033yxfrdjfglzk0j2k";
   };
 
   nativeBuildInputs = [ cmake extra-cmake-modules makeWrapper ];
+
   buildInputs = [
-    boost doxygen graphviz loki mysql.connector-c openssl postgresql qscintillaLib qtbase
+    boost doxygen graphviz loki mysql.connector-c openssl postgresql qscintilla qtbase qttools
   ];
 
   preConfigure = ''
@@ -28,14 +35,18 @@ in mkDerivation rec {
     sed -i \
       's|/usr/bin/dot|${lib.getBin graphviz}/bin/dot|' \
       extlibs/libermodel/dotgraph.cpp
+
+    if [ ! -e ${qsctLibFile} ]; then
+      echo "Unable to find: ${qsctLibFile}"
+      exit 1
+    fi
   '';
 
   cmakeFlags = [
     "-DWANT_INTERNAL_LOKI=0"
     "-DWANT_INTERNAL_QSCINTILLA=0"
-    # cmake/modules/FindQScintilla.cmake looks in qtbase and for the wrong library name
-    "-DQSCINTILLA_INCLUDE_DIR=${qscintillaLib}/include"
-    "-DQSCINTILLA_LIBRARY=${qscintillaLib}/lib/libqscintilla2.so"
+    "-DQSCINTILLA_INCLUDE_DIR=${qscintilla}/include"
+    "-DQSCINTILLA_LIBRARY=${qsctLibFile}"
     "-DENABLE_DB2=0"
     "-DENABLE_ORACLE=0"
     "-DENABLE_TERADATA=0"
