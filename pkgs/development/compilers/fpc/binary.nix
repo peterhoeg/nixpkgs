b1fe9059e233cb538455a958c3bf129f75ebf609
Peter Hoeg <peter@hoeg.com>
2019-03-31 14:24:32 +0800

    fpc

--- pkgs/development/compilers/fpc/binary.nix
+++ pkgs/development/compilers/fpc/binary.nix
@@ -1,24 +1,43 @@
 { stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-  name = "fpc-2.6.0-binary";
+let
+  system = stdenv.hostPlatform.system;
 
-  src =
-    if stdenv.hostPlatform.system == "i686-linux" then
+  generic = version: sha256s: stdenv.mkDerivation {
+    name = "fpc-${version}-binary";
+
+    src = if stdenv.hostPlatform.system == "i686-linux" then
       fetchurl {
-        url = "mirror://sourceforge/project/freepascal/Linux/2.6.0/fpc-2.6.0.i386-linux.tar";
-        sha256 = "08yklvrfxvk59bxsd4rh1i6s3cjn0q06dzjs94h9fbq3n1qd5zdf";
+        url = "mirror://sourceforge/project/freepascal/Linux/${version}/fpc-${version}.i386-linux.tar";
+        sha256 = sha256s."${system}";
       }
     else if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
-        url = "mirror://sourceforge/project/freepascal/Linux/2.6.0/fpc-2.6.0.x86_64-linux.tar";
-        sha256 = "0k9vi75k39y735fng4jc2vppdywp82j4qhzn7x4r6qjkad64d8lx";
+        url = "mirror://sourceforge/project/freepascal/Linux/${version}/fpc-${version}.x86_64-linux.tar";
+        sha256 = sha256s."${system}";
       }
     else throw "Not supported on ${stdenv.hostPlatform.system}.";
 
-  builder = ./binary-builder.sh;
+    builder = ./binary-builder.sh;
 
-  meta = {
-    description = "Free Pascal Compiler from a binary distribution";
+    meta = {
+      description = "Free Pascal Compiler from a binary distribution";
+    };
   };
-} 
+
+in {
+  fpc-binary_260 = generic "2.6.0" {
+    i686-linux   = "08yklvrfxvk59bxsd4rh1i6s3cjn0q06dzjs94h9fbq3n1qd5zdf";
+    x86_64-linux = "0k9vi75k39y735fng4jc2vppdywp82j4qhzn7x4r6qjkad64d8lx";
+  };
+
+  fpc-binary_300 = generic "3.0.0" {
+    i686-linux   = "11yklvrfxvk59bxsd4rh1i6s3cjn0q06dzjs94h9fbq3n1qd5zdf";
+    x86_64-linux = "1m2xx3nda45cb3zidbjgdr8kddd19zk0khvp7xxdlclszkqscln9";
+  };
+
+  fpc-binary_304 = generic "3.0.4" {
+    i686-linux   = "1111lvrfxvk59bxsd4rh1i6s3cjn0q06dzjs94h9fbq3n1qd5zdf";
+    x86_64-linux = "0xzxh689iyjfmkqkhcqg9plrjmdx82hbyywyyc7jm0n92fpmp5ky";
+  };
+}
--- pkgs/development/compilers/fpc/default.nix
+++ pkgs/development/compilers/fpc/default.nix
@@ -1,46 +1,86 @@
-{ stdenv, fetchurl, gawk }:
+{ stdenv, fetchurl, makeWrapper, writeText, binutils, gawk }:
 
-let startFPC = import ./binary.nix { inherit stdenv fetchurl; }; in
+let
+  inherit (import ./binary.nix { inherit stdenv fetchurl; }) fpc-binary_260 fpc-binary_300 fpc-binary_304;
 
-stdenv.mkDerivation rec {
-  version = "3.0.0";
-  name = "fpc-${version}";
-
-  src = fetchurl {
-    url = "mirror://sourceforge/freepascal/fpcbuild-${version}.tar.gz";
-    sha256 = "1v40bjp0kvsi8y0mndqvvhnsqjfssl2w6wpfww51j4rxblfkp4fm";
-  };
-
-  buildInputs = [ startFPC gawk ];
-
-  preConfigure =
-    if stdenv.hostPlatform.system == "i686-linux" || stdenv.hostPlatform.system == "x86_64-linux" then ''
-      sed -e "s@'/lib/ld-linux[^']*'@'''@" -i fpcsrc/compiler/systems/t_linux.pas
-      sed -e "s@'/lib64/ld-linux[^']*'@'''@" -i fpcsrc/compiler/systems/t_linux.pas
-    '' else "";
-
-  makeFlags = "NOGDB=1 FPC=${startFPC}/bin/fpc";
-
-  installFlags = "INSTALL_PREFIX=\${out}";
-
-  postInstall = ''
-    for i in $out/lib/fpc/*/ppc*; do
-      ln -fs $i $out/bin/$(basename $i)
-    done
-    mkdir -p $out/lib/fpc/etc/
-    $out/lib/fpc/*/samplecfg $out/lib/fpc/${version} $out/lib/fpc/etc/
+  helloWorld = writeText "hello.pas" ''
+    program Hello;
+    begin
+      writeln('Hello, world.');
+    end.
   '';
 
-  passthru = {
-    bootstrap = startFPC;
+  generic = version: sha256: binary: stdenv.mkDerivation rec {
+    name = "fpc-${version}";
+
+    src = fetchurl {
+      url = "mirror://sourceforge/freepascal/fpcbuild-${version}.tar.gz";
+      inherit sha256;
+    };
+
+    buildInputs = [ binutils ];
+
+    nativeBuildInputs = [ binary makeWrapper gawk ];
+
+    # We were previously only doing this when building on Linux but we might as well do it all the time.
+    # It shouldn't in any way interfere with building on other platforms.
+    preConfigure = ''
+      sed -i fpcsrc/compiler/systems/t_linux.pas \
+        -e "s@'/lib/@'${stdenv.cc.libc}/lib/@"   \
+        -e "s@'/lib64/@'${stdenv.cc.libc}/lib/@"
+    '';
+
+    makeFlags = [
+      "NOGDB=1"
+      "FPC=${binary}/bin/fpc"
+      # "LD=${binutils}/bin/ld.gold"
+      ''OPT="-gl"''
+    ];
+
+    installFlags = [
+      "INSTALL_PREFIX=$(out)"
+    ];
+
+    # NIX_LDFLAGS = [
+    #   "-rpath ${stdenv.cc.cc.lib}/lib"
+    #   "-rpath ${stdenv.cc.libc}/lib"
+    # ];
+
+    postInstall = ''
+      for i in $out/lib/fpc/*/ppc*; do
+        ln -fs $i $out/bin/$(basename $i)
+      done
+      mkdir -p $out/lib/fpc/etc/
+      $out/lib/fpc/*/samplecfg $out/lib/fpc/${version} $out/lib/fpc/etc/
+
+      wrapProgram $out/bin/fpc \
+        --prefix PATH : ${stdenv.lib.makeBinPath [ binutils ]}
+    '';
+
+    doInstallCheck = true;
+
+    installCheckPhase = ''
+      $out/bin/fpc -o./hello ${helloWorld}
+      ./hello
+    '';
+
+    passthru = {
+      bootstrap = binary;
+    };
+
+    meta = with stdenv.lib; {
+      description = "Free Pascal Compiler from a source distribution";
+      homepage = https://www.freepascal.org;
+      maintainers = with maintainers; [ raskin ];
+      license = with licenses; [ gpl2 lgpl2 ];
+      platforms = platforms.linux;
+      inherit version;
+    };
   };
 
-  meta = with stdenv.lib; {
-    description = "Free Pascal Compiler from a source distribution";
-    homepage = https://www.freepascal.org;
-    maintainers = [ maintainers.raskin ];
-    license = with licenses; [ gpl2 lgpl2 ];
-    platforms = platforms.linux;
-    inherit version;
-  };
+in rec {
+  fpc_300 = generic "3.0.0" "1v40bjp0kvsi8y0mndqvvhnsqjfssl2w6wpfww51j4rxblfkp4fm" fpc-binary_304;
+  fpc_302 = generic "3.0.2" "0x8vajb3brmh078hxz0pydym1wbxf1dxca7lzxlh268z6q5fsqgj" fpc-binary_304;
+  fpc_304 = generic "3.0.4" "0xjyhlhz846jbnp12y68c7nq4xmp4i65akfbrjyf3r62ybk18rgn" fpc-binary_304;
+  fpc = fpc_304;
 }
