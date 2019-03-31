b095f5139baeb24a6e2365c10904db0558d9a2ba
Peter Hoeg <peter@hoeg.com>
2019-03-31 14:24:39 +0800

    laz

--- pkgs/development/compilers/fpc/lazarus.nix
+++ pkgs/development/compilers/fpc/lazarus.nix
@@ -1,7 +1,9 @@
 { stdenv, fetchurl, makeWrapper
 , fpc, gtk2, glib, pango, atk, gdk_pixbuf
 , libXi, xorgproto, libX11, libXext
+, withQt ? false, qt5 ? null
 }:
+
 stdenv.mkDerivation rec {
   name = "lazarus-${version}";
   version = "1.8.4";
@@ -14,35 +16,55 @@ stdenv.mkDerivation rec {
   buildInputs = [
     fpc gtk2 glib libXi xorgproto
     libX11 libXext pango atk
-    stdenv.cc makeWrapper gdk_pixbuf
+    stdenv.cc gdk_pixbuf
   ];
+
+  nativeBuildInputs = [ makeWrapper ];
 
   makeFlags = [
     "FPC=fpc"
     "PP=fpc"
     "REQUIRE_PACKAGES+=tachartlazaruspkg"
     "bigide"
+    "LAZARUS_INSTALL_DIR==$(out)/share/lazarus/"
+    "INSTALL_PREFIX=$(out)/"
+  ];
+
+  LCL_PLATFORM = if withQt then "qt5" else "gtk2";
+
+  NIX_LDFLAGS = [
+    "-L${stdenv.cc.cc.lib}/lib"
+    "-lXi"
+    "-lX11"
+    "-lglib-2.0"
+    "-lgtk-x11-2.0"
+    "-lgdk-x11-2.0"
+    "-lc"
+    "-lXext"
+    "-lpango-1.0"
+    "-latk-1.0"
+    "-lgdk_pixbuf-2.0"
+    "-lcairo"
+    "-lgcc_s"
   ];
 
   preBuild = ''
-    export makeFlags="$makeFlags LAZARUS_INSTALL_DIR=$out/share/lazarus/ INSTALL_PREFIX=$out/"
-    export NIX_LDFLAGS="$NIX_LDFLAGS -L${stdenv.cc.cc.lib}/lib -lXi -lX11 -lglib-2.0 -lgtk-x11-2.0 -lgdk-x11-2.0 -lc -lXext -lpango-1.0 -latk-1.0 -lgdk_pixbuf-2.0 -lcairo -lgcc_s"
-    export LCL_PLATFORM=gtk2
-    mkdir -p $out/share "$out/lazarus"
+    mkdir -p $out/{lazarus,share}
     tar xf ${fpc.src} --strip-components=1 -C $out/share -m
     sed -e 's@/usr/fpcsrc@'"$out/share/fpcsrc@" -i ide/include/unix/lazbaseconf.inc
   '';
 
   postInstall = ''
-    wrapProgram $out/bin/startlazarus --prefix NIX_LDFLAGS ' ' "'$NIX_LDFLAGS'" \
+    wrapProgram $out/bin/startlazarus \
+      --prefix NIX_LDFLAGS  ' ' "'$NIX_LDFLAGS'" \
       --prefix LCL_PLATFORM ' ' "'$LCL_PLATFORM'"
   '';
 
   meta = with stdenv.lib; {
-    description = "Lazarus graphical IDE for FreePascal language";
-    homepage = http://www.lazarus.freepascal.org;
+    description = "Lazarus graphical IDE for the FreePascal language";
+    homepage = https://www.lazarus.freepascal.org;
     license = licenses.gpl2Plus ;
+    maintainers = with maintainers; [ raskin ];
     platforms = platforms.linux;
-    maintainers = [ maintainers.raskin ];
   };
 }
