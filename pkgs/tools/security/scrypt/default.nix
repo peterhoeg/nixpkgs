daeefda81aa8a605b83311e3ff5c9e1dab109457
Peter Hoeg <peter@hoeg.com>
2019-08-21 17:52:23 +0800

    scrypt: name -> pname

--- pkgs/tools/security/scrypt/default.nix
+++ pkgs/tools/security/scrypt/default.nix
@@ -1,15 +1,13 @@
 { stdenv, fetchurl, openssl }:
 
 stdenv.mkDerivation rec {
-  name = "scrypt-${version}";
+  pname = "scrypt";
   version = "1.2.1";
 
   src = fetchurl {
-    url = "https://www.tarsnap.com/scrypt/${name}.tgz";
+    url = "https://www.tarsnap.com/scrypt/${pname}-${version}.tgz";
     sha256 = "0xy5yhrwwv13skv9im9vm76rybh9f29j2dh4hlh2x01gvbkza8a6";
   };
-
-  buildInputs = [ openssl ];
 
   patchPhase = ''
     for f in Makefile.in autotools/Makefile.am libcperciva/cpusupport/Build/cpusupport.sh ; do
@@ -17,11 +15,15 @@ stdenv.mkDerivation rec {
     done
   '';
 
+  buildInputs = [ openssl ];
+
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
     description = "Encryption utility";
-    homepage    = https://www.tarsnap.com/scrypt.html;
+    homepage    = "https://www.tarsnap.com/scrypt.html";
     license     = licenses.bsd2;
-    platforms   = platforms.all;
     maintainers = with maintainers; [ thoughtpolice ];
+    platforms   = platforms.all;
   };
 }
