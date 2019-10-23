6a1b56f8f50ca7c567c36079ce7a9471c2a1fb97
Peter Hoeg <peter@hoeg.com>
2019-10-23 15:00:19 +0800

    bundler: 1.17.3 -> 2.0.2

--- pkgs/development/ruby-modules/bundler/default.nix
+++ pkgs/development/ruby-modules/bundler/default.nix
@@ -4,8 +4,8 @@ buildRubyGem rec {
   inherit ruby;
   name = "${gemName}-${version}";
   gemName = "bundler";
-  version = "1.17.3";
-  source.sha256 = "0ln3gnk7cls81gwsbxvrmlidsfd78s6b2hzlm4d4a9wbaidzfjxw";
+  version = "2.0.2";
+  source.sha256 = "1l5203dhf51dnchrd2lxlvqgikx763jz323imhrbhwm0x3yf2ajc";
   dontPatchShebangs = true;
 
   postFixup = ''
