79cab73847f1dec8f7e238ff27f23c0ab3a8d975
Peter Hoeg <peter@hoeg.com>
2019-11-18 14:45:43 +0800

    gource: 0.49 -> 0.50

--- pkgs/applications/version-management/gource/default.nix
+++ pkgs/applications/version-management/gource/default.nix
@@ -1,17 +1,22 @@
-{ stdenv, fetchurl, SDL2, ftgl, pkgconfig, libpng, libjpeg, pcre
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig
+, SDL2, ftgl, libpng, libjpeg, pcre
 , SDL2_image, freetype, glew, libGLU_combined, boost, glm
 }:
 
 stdenv.mkDerivation rec {
-  version = "0.49";
+  version = "0.50";
   pname = "gource";
 
-  src = fetchurl {
-    url = "https://github.com/acaudwell/Gource/releases/download/${pname}-${version}/${pname}-${version}.tar.gz";
-    sha256 = "12hf5ipcsp9dxsqn84n4kr63xaiskrnf5a084wr29qk171lj7pd9";
+  src = fetchFromGitHub {
+    owner = "acaudwell";
+    repo = "Gource";
+    rev = "gource-${version}";
+    sha256 = "02lyngr16fj88x931cd9ymkmb3bx5mx68ydqnvnss0qcqvxip6ps";
+    fetchSubmodules = true;
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+
   buildInputs = [
     glew SDL2 ftgl libpng libjpeg pcre SDL2_image libGLU_combined
     boost glm freetype
@@ -22,8 +27,8 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    homepage = https://gource.io/;
     description = "A Software version control visualization tool";
+    homepage = "https://gource.io/";
     license = licenses.gpl3Plus;
     longDescription = ''
       Software projects are displayed by Gource as an animated tree with
@@ -35,7 +40,7 @@ stdenv.mkDerivation rec {
       Mercurial and Bazaar and SVN. Gource can also parse logs produced
       by several third party tools for CVS repositories.
     '';
-    platforms = platforms.unix;
     maintainers = with maintainers; [ pSub ];
+    platforms = platforms.unix;
   };
 }
