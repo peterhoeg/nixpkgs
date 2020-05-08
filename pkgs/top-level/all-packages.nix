3750b8a44ce5fe9d1fc5be3af7cfdad814cb3fee
Stefan Frijters <sfrijters@gmail.com>
2020-05-08 23:26:06 +0200

    urserver: init at 3.6.0.745

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26926,4 +26926,6 @@ in
   gpio-utils = callPackage ../os-specific/linux/kernel/gpio-utils.nix { };
 
   navidrome = callPackage ../servers/misc/navidrome {};
+
+  urserver = callPackage ../servers/urserver { };
 }
