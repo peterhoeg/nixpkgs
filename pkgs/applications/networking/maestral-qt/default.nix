040857d8ceb565d134f51591e6005652585b20ca
Stefan Frijters <sfrijters@gmail.com>
2020-05-15 13:31:35 +0200

    maestral: Clean up

--- pkgs/applications/networking/maestral-qt/default.nix
+++ pkgs/applications/networking/maestral-qt/default.nix
@@ -5,13 +5,14 @@
 , wrapQtAppsHook
 }:
 
-let pbi = with python3.pkgs; [
+let
+  pbi = with python3.pkgs; [
     bugsnag
     click
     markdown2
     maestral
     pyqt5
-    ] ++ (with python.pkgs; [
+  ] ++ (with python.pkgs; [
     blinker
     bugsnag
     click
@@ -24,13 +25,11 @@ let pbi = with python3.pkgs; [
     requests
     u-msgpack-python
     watchdog
-    ]) ++ stdenv.lib.optionals stdenv.isLinux [
+  ]) ++ stdenv.lib.optionals stdenv.isLinux [
     sdnotify
     systemd
   ];
-
 in
-
 python3.pkgs.buildPythonApplication rec {
   pname = "maestral-qt";
   version = "1.0.0";
@@ -48,33 +47,20 @@ python3.pkgs.buildPythonApplication rec {
 
   nativeBuildInputs = [ wrapQtAppsHook ];
 
-  # pythonPath = with python3.pkgs; [
-  #   pyqt5
-  # ];
-
   makeWrapperArgs = [
     # Firstly, add all necessary QT variables
     "\${qtWrapperArgs[@]}"
 
-    # # Then, add the installed scripts/ directory to the python path
+    # Add the installed directories to the python path so the daemon can find them
     "--prefix" "PYTHONPATH" ":" "${stdenv.lib.concatStringsSep ":" (map (p: p + "/lib/${python3.libPrefix}/site-packages") (python3.pkgs.requiredPythonModules pbi))}"
     "--prefix" "PYTHONPATH" ":" "${python3.pkgs.maestral}/lib/${python3.libPrefix}/site-packages"
-    # "--prefix" "PYTHONPATH" ":" "${python3.pkgs.Pyro5}/lib/${python3.libPrefix}/site-packages"
-
-    # Finally, move to directory that contains data
-    # "--run" "\"cd $out/share/${pname}\""
   ];
-
-  # postInstall = lib.optionalString withGui ''
-  #   makeQtWrapper $out/bin/maestral $out/bin/maestral-gui \
-  #     --add-flags gui
-  # '';
 
   # no tests
   doCheck = false;
 
   meta = with lib; {
-    description = "Open-source Dropbox client for macOS and Linux";
+    description = "GUI front-end for maestral (an open-source Dropbox client) for Linux";
     license = licenses.mit;
     maintainers = with maintainers; [ peterhoeg ];
     platforms = platforms.linux;
