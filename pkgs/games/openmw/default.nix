05dcc9ae5ffc18c9ebd856496032f1a449749d2a
Peter Hoeg <peter@hoeg.com>
2020-06-16 10:16:34 +0800

    openmw: 0.45 -> 0.46

--- pkgs/games/openmw/default.nix
+++ pkgs/games/openmw/default.nix
@@ -3,27 +3,34 @@
 
 let
   openscenegraph_ = openscenegraph.overrideDerivation (self: {
+    name = "openmw-openscenegraph-3.4.2020-04-26";
+
     src = fetchFromGitHub {
       owner = "OpenMW";
       repo = "osg";
-      rev = "2b4c8e37268e595b82da4b9aadd5507852569b87";
-      sha256 = "0admnllxic6dcpic0h100927yw766ab55dix002vvdx36i6994jb";
+      rev = "8b07809fa674ecffe77338aaea2e223b3aadff0e";
+      sha256 = "0m6qrd55rqn0p0fpml8605cgrg9p4m1x37yd29781a2mbm0nz130";
     };
+
+    cmakeFlags = self.cmakeFlags ++ [
+      "-Wno-dev"
+      "-DOpenGL_GL_PREFERENCE=GLVND"
+    ];
   });
+
 in mkDerivationWith stdenv.mkDerivation rec {
-  version = "0.45.0";
+  version = "0.46.0";
   pname = "openmw";
 
   src = fetchFromGitHub {
     owner = "OpenMW";
     repo = "openmw";
     rev = "${pname}-${version}";
-    sha256 = "1r87zrsnza2v9brksh809zzqj6zhk5xj15qs8iq11v1bscm2a2j4";
+    sha256 = "0rm32zsmxvr6b0jjihfj543skhicbw5kg6shjx312clhlm035w2x";
   };
 
-  enableParallelBuilding = true;
-
   nativeBuildInputs = [ pkgconfig ];
+
   buildInputs = [ cmake boost ffmpeg_3 bullet mygui openscenegraph_ SDL2 unshield openal libXt qtbase ];
 
   cmakeFlags = [
@@ -34,7 +41,7 @@ in mkDerivationWith stdenv.mkDerivation rec {
     description = "An unofficial open source engine reimplementation of the game Morrowind";
     homepage = "http://openmw.org";
     license = licenses.gpl3;
-    platforms = platforms.linux;
     maintainers = with maintainers; [ abbradar ];
+    platforms = platforms.linux;
   };
 }
