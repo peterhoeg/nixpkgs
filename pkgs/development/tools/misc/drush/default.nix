e050715084ba90bdaa12a94b3eeb501d1249c236
Peter Hoeg <peter@hoeg.com>
2020-12-09 15:37:33 +0800

    WIP

--- pkgs/development/tools/misc/drush/default.nix
+++ pkgs/development/tools/misc/drush/default.nix
@@ -1,38 +1,42 @@
-{ stdenv, fetchurl, php73, which, makeWrapper, bash, coreutils, ncurses }:
-
-stdenv.mkDerivation rec {
-  name = "drush-6.1.0";
-
-  meta = with stdenv.lib; {
-    description = "Command-line shell and Unix scripting interface for Drupal";
-    homepage    = "https://github.com/drush-ops/drush";
-    license     = licenses.gpl2;
-    maintainers = with maintainers; [ lovek323 ];
-    platforms   = platforms.all;
-  };
-
-  src = fetchurl {
-    url    = "https://github.com/drush-ops/drush/archive/6.1.0.tar.gz";
-    sha256 = "1jgnc4jjyapyn04iczvcz92ic0vq8d1w8xi55ismqyy5cxhqj6bp";
-  };
-
+{ stdenv, fetchurl, fetchFromGitHub, php, which, makeWrapper, bash, coreutils, ncurses }:
+let
   consoleTable = fetchurl {
-    url    = "http://download.pear.php.net/package/Console_Table-1.1.3.tgz";
+    url = "http://download.pear.php.net/package/Console_Table-1.1.3.tgz";
     sha256 = "07gbjd7m1fj5dmavr0z20vkqwx1cz2522sj9022p257jifj1yl76";
+  };
+
+  path = stdenv.lib.makeBinPath [ which php bash coreutils ncurses ];
+
+in
+stdenv.mkDerivation rec {
+  pname = "drush";
+  version = "10.3.6";
+
+  src = fetchFromGitHub {
+    owner = "drush-ops";
+    repo = "drush";
+    rev = version;
+    sha256 = "sha256-45I0ys8PONlvYA6iXDK5ehUtI5BKCDhNsCoqXw5nIfw=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     # install libraries
-    cd lib
-    tar -xf ${consoleTable}
-    cd ..
+    # tar -xf ${consoleTable} -C lib
 
-    mkdir -p "$out"
-    cp -r . "$out/src"
-    mkdir "$out/bin"
-    wrapProgram "$out/src/drush" --prefix PATH : "${stdenv.lib.makeBinPath [ which php73 bash coreutils ncurses ]}"
+    mkdir -p $out/bin
+    cp -r . $out/src
+    wrapProgram $out/src/drush \
+      --prefix PATH : ${path}
     ln -s "$out/src/drush" "$out/bin/drush"
   '';
+
+  meta = with stdenv.lib; {
+    description = "Command-line shell and Unix scripting interface for Drupal";
+    homepage = "https://github.com/drush-ops/drush";
+    license = licenses.gpl2;
+    maintainers = with maintainers; [ lovek323 ];
+    platforms = platforms.all;
+  };
 }
