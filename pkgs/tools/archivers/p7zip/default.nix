65374e872fc77426053f2ebb6b6cb0b55e8a0154
Peter Hoeg <peter@hoeg.com>
2021-01-14 13:12:55 +0800

    p7zip: 17.01 -> 17.03

--- pkgs/tools/archivers/p7zip/default.nix
+++ pkgs/tools/archivers/p7zip/default.nix
@@ -1,35 +1,17 @@
-{ stdenv, fetchFromGitHub, fetchpatch, lib, enableUnfree ? false }:
+{ stdenv, fetchFromGitHub, fetchpatch, lib, cmake, enableUnfree ? false }:
 
 stdenv.mkDerivation rec {
   pname = "p7zip";
   version = "17.03";
 
   src = fetchFromGitHub {
-    owner  = "szcnick";
-    repo   = pname;
-    rev    = "v${version}";
-    sha256 = "0zgpa90z5p30jbpqydiig1h8hn41c76n2x26rh8cc92xw72ni33d";
+    owner = "jinfeihan57";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-bYxoxeFdJMYQzEZ0Yc1hgViIYHgxNo/vkmDc8kFS930=";
   };
 
-  # Default makefile is full of impurities on Darwin. The patch doesn't hurt Linux so I'm leaving it unconditional
-  postPatch = ''
-    sed -i '/CC=\/usr/d' makefile.macosx_llvm_64bits
-    chmod +x install.sh
-
-    # I think this is a typo and should be CXX? Either way let's kill it
-    sed -i '/XX=\/usr/d' makefile.macosx_llvm_64bits
-  '' + lib.optionalString (stdenv.buildPlatform != stdenv.hostPlatform) ''
-    substituteInPlace makefile.machine \
-      --replace 'CC=gcc'  'CC=${stdenv.cc.targetPrefix}gcc' \
-      --replace 'CXX=g++' 'CXX=${stdenv.cc.targetPrefix}g++'
-  '' + lib.optionalString (!enableUnfree) ''
-    # Remove non-free RAR source code
-    # (see DOC/License.txt, https://fedoraproject.org/wiki/Licensing:Unrar)
-    rm -r CPP/7zip/Compress/Rar*
-    find . -name makefile'*' -exec sed -i '/Rar/d' {} +
-  '';
-
-  makeFlags = [ "DEST_HOME=${placeholder "out"}" ];
+  sourceRoot = "source/CPP/7zip/CMAKE";
 
   preConfigure = ''
     buildFlags=all3
@@ -37,18 +19,30 @@ stdenv.mkDerivation rec {
     cp makefile.macosx_llvm_64bits makefile.machine
   '';
 
-  enableParallelBuilding = true;
+  # Remove non-free RAR source code
+  # (see DOC/License.txt, https://fedoraproject.org/wiki/Licensing:Unrar)
+  postPatch = lib.optionalString (!enableUnfree) ''
+    rm -r Rar
+    sed -i '/Rar/d' CMakeLists.txt
+  '';
+
+  nativeBuildInputs = [ cmake ];
 
   setupHook = ./setup-hook.sh;
 
   NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-error=c++11-narrowing";
 
-  meta = {
-    homepage = "https://github.com/szcnick/p7zip";
+  # for reasons unknown, the Makefile with the install info is written to bin with the binary artifacts
+  preInstall = ''
+    cd bin
+  '';
+
+  meta = with lib; {
     description = "A new p7zip fork with additional codecs and improvements (forked from https://sourceforge.net/projects/p7zip/)";
-    platforms = lib.platforms.unix;
-    maintainers = [ lib.maintainers.raskin ];
+    homepage = "https://github.com/jinfeihan57/p7zip";
     # RAR code is under non-free UnRAR license, but we remove it
     license = if enableUnfree then lib.licenses.unfree else lib.licenses.lgpl2Plus;
+    maintainers = with maintainers; [ raskin ];
+    platforms = platforms.unix;
   };
 }
