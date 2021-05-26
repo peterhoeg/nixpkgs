12da54a9dcc49bd4d55a32dbbc6071a15a35acd6
Peter Hoeg <peter@hoeg.com>
2021-05-26 22:15:19 +0800

    WIP

--- pkgs/development/libraries/plasma-wayland-protocols/default.nix
+++ pkgs/development/libraries/plasma-wayland-protocols/default.nix
@@ -1,20 +1,21 @@
 { mkDerivation, fetchurl, lib
 , extra-cmake-modules
-, qtbase
 }:
 
 mkDerivation rec {
   pname = "plasma-wayland-protocols";
-  version = "1.2.1";
+  version = "1.3.0";
 
   src = fetchurl {
-    url = "mirror://kde/stable/${pname}/${pname}-v${version}.tar.xz";
-    sha256 = "sha256-KHuQkD+afzlMdedcsYdCaGLq9kqS8b5+LvaOmf2Muqo=";
+    urls = [
+      "mirror://kde/stable/${pname}/${pname}-${version}.tar.xz"
+      # v1.2.1 has a v version prefix
+      "mirror://kde/stable/${pname}/${pname}-v${version}.tar.xz"
+    ];
+    sha256 = "sha256-DaojYvLg0V954OAG6NfxkI6I43tcUgi0DJyw1NbcqbU=";
   };
 
   nativeBuildInputs = [ extra-cmake-modules ];
-
-  buildInputs = [ qtbase ];
 
   meta = {
     description = "Plasma Wayland Protocols";
