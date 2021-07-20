1a40d26ba58b966d12a333c33dd025365c2e9a6c
Peter Hoeg <peter@hoeg.com>
2021-07-20 20:35:58 +0800

    inform7: drive-by cleanup

--- pkgs/development/compilers/inform7/default.nix
+++ pkgs/development/compilers/inform7/default.nix
@@ -1,24 +1,38 @@
-{ lib, stdenv, fetchzip, coreutils, perl, gnutar, gzip }:
+{ lib, stdenv, fetchzip, perlPackages }:
+
 let
-  version = "6M62";
-in stdenv.mkDerivation {
+  arch = {
+    x86_64-linux = "x86_64";
+    i686-linux = "i386";
+  }.${stdenv.hostPlatform.system};
+
+in
+perlPackages.buildPerlPackage rec {
   pname = "inform7";
-  inherit version;
-  buildInputs = [ perl coreutils gnutar gzip ];
+  version = "6M62";
+
   src = fetchzip {
-    url = "http://inform7.com/download/content/6M62/I7_6M62_Linux_all.tar.gz";
+    url = "http://inform7.com/download/content/${version}/I7_${version}_Linux_all.tar.gz";
     sha256 = "0bk0pfymvsn1g8ci0pfdw7dgrlzb232a8pc67y2xk6zgpf3m41vj";
   };
-  preConfigure = "touch Makefile.PL";
-  buildPhase = "";
-  installPhase = ''
-    mkdir -p $out
-    pushd $src
-    ./install-inform7.sh --prefix $out
-    popd
 
-    substituteInPlace "$out/bin/i7" \
-      --replace "/usr/bin/perl" "${perl}/bin/perl"
+  outputs = [ "out" ];
+
+  postPatch = "touch Makefile.PL";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+
+    for f in inform7-*_{all,${arch}}.tar.gz; do
+      tar xzf $f --directory=$out
+    done
+
+    substituteInPlace $out/bin/i7 \
+      --replace /usr/local $out
+
+    runHook postInstall
   '';
 
   meta = with lib; {
