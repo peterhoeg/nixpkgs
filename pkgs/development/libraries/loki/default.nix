597d0b6b80581d532cb0232820f4d35352a28228
Peter Hoeg <peter@hoeg.com>
2022-02-18 20:40:08 +0800

    loki: minor cleanups

--- pkgs/development/libraries/loki/default.nix
+++ pkgs/development/libraries/loki/default.nix
@@ -6,21 +6,34 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/loki-lib/Loki/Loki%20${version}/loki-${version}.tar.gz";
-    sha256 = "1xhwna961fl4298ac5cc629x5030zlw31vx4h8zws290amw5860g";
+    sha256 = "sha256-DxhUeFUgCc0/gqTvMDj9YIDSkzCMFaZQEoS6YJKyHPY=";
   };
 
   buildPhase = ''
+    runHook preBuild
+
     substituteInPlace Makefile.common --replace /usr $out
     make build-shared
+
+    runHook postBuild
   '';
 
   enableParallelBuilding = true;
+
+  # it's noisy and last release was in 2009, so it's bound to happen - just shut
+  # up the warnings.
+  NIX_CFLAGS_COMPILE = map (e: "-Wno-${e}") [
+    "conversion"
+    "deprecated"
+    "deprecated-declarations"
+    "unused-local-typedefs"
+  ];
 
   meta = with lib; {
     description = "A C++ library of designs, containing flexible implementations of common design patterns and idioms";
     homepage = "http://loki-lib.sourceforge.net";
     license = licenses.mit;
-    platforms = platforms.all;
     maintainers = with maintainers; [ peterhoeg ];
+    platforms = platforms.all;
   };
 }
