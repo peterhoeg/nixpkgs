6db5c15c8294017456d8ee27a48d4a7b3d51464e
Peter Hoeg <peter@hoeg.com>
2022-09-15 16:57:35 +0800

    nixos/unifi: clean up

--- nixos/tests/unifi.nix
+++ nixos/tests/unifi.nix
@@ -9,6 +9,9 @@ with import ../lib/testing-python.nix { inherit system pkgs; };
 with pkgs.lib;
 
 let
+  httpPort = 8080;
+  httpsPort = 18443;
+
   makeAppTest = unifi: makeTest {
     name = "unifi-controller-${unifi.version}";
     meta = with pkgs.lib.maintainers; {
@@ -20,15 +23,18 @@ let
         enable = true;
         unifiPackage = unifi;
         openFirewall = false;
+        inherit httpPort httpsPort;
       };
     };
 
     testScript = ''
       server.wait_for_unit("unifi.service")
-      server.wait_until_succeeds("curl -Lk https://localhost:8443 >&2", timeout=300)
+      server.wait_until_succeeds("curl -Lk https://localhost:${toString httpsPort} >&2", timeout=300)
     '';
   };
-in with pkgs; {
+
+in
+with pkgs; {
   unifiLTS = makeAppTest unifiLTS;
   unifi5 = makeAppTest unifi5;
   unifi6 = makeAppTest unifi6;
