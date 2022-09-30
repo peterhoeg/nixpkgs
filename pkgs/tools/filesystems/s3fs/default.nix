c1d7646bb026b2ef41807a6a88cc8c346a67338a
Peter Hoeg <peter@hoeg.com>
2022-09-30 13:38:25 +0800

    WIP

--- pkgs/tools/filesystems/s3fs/default.nix
+++ pkgs/tools/filesystems/s3fs/default.nix
@@ -1,17 +1,23 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, curl, openssl, libxml2, fuse }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, curl, mailcap, openssl, libxml2, fuse }:
 
 stdenv.mkDerivation rec {
   pname = "s3fs-fuse";
   version = "1.91";
 
   src = fetchFromGitHub {
-    owner  = "s3fs-fuse";
-    repo   = "s3fs-fuse";
-    rev    = "v${version}";
+    owner = "s3fs-fuse";
+    repo = "s3fs-fuse";
+    rev = "v${version}";
     sha256 = "sha256-41IgUgpVZiIzi3N5kgX7PAhgnd+i/FH1o8t5y3Uw14g=";
   };
 
-  buildInputs = [ curl openssl libxml2 fuse ];
+  postPatch = ''
+    substituteInPlace src/curl.cpp \
+      --replace /etc/mime.types ${mailcap}/etc/mime.types
+  '';
+
+  buildInputs = [ curl openssl libxml2 fuse mailcap ];
+
   nativeBuildInputs = [ autoreconfHook pkg-config ];
 
   configureFlags = [
@@ -22,10 +28,12 @@ stdenv.mkDerivation rec {
     ln -s $out/bin/s3fs $out/bin/mount.s3fs
   '';
 
+  enableParallelBuilding = true;
+
   meta = with lib; {
-    broken = stdenv.isDarwin;
     description = "Mount an S3 bucket as filesystem through FUSE";
     license = licenses.gpl2;
     platforms = platforms.linux ++ platforms.darwin;
+    broken = stdenv.isDarwin;
   };
 }
