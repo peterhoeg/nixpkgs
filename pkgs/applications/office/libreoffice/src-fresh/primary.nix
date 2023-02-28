e3b96f61d3454d1e44e25b55bf43a821e63c512a
Peter Hoeg <peter@hoeg.com>
2023-02-28 15:25:34 +0800

    libreoffice-fresh: 7.4.2.3 -> 7.5.1.2

--- pkgs/applications/office/libreoffice/src-fresh/primary.nix
+++ pkgs/applications/office/libreoffice/src-fresh/primary.nix
@@ -1,15 +1,15 @@
 { fetchurl }:
 
 rec {
-  fetchSrc = {name, sha256}: fetchurl {
+  fetchSrc = { name, sha256 }: fetchurl {
     url = "https://download.documentfoundation.org/libreoffice/src/${subdir}/libreoffice-${name}-${version}.tar.xz";
     inherit sha256;
   };
 
   major = "7";
-  minor = "4";
-  patch = "2";
-  tweak = "3";
+  minor = "5";
+  patch = "1";
+  tweak = "2";
 
   subdir = "${major}.${minor}.${patch}";
 
@@ -17,13 +17,13 @@ rec {
 
   src = fetchurl {
     url = "https://download.documentfoundation.org/libreoffice/src/${subdir}/libreoffice-${version}.tar.xz";
-    hash = "sha256-gsH/4C8u2O4UUan2fDUzWyemONtZH5vFOe/4arFN2Vo=";
+    hash = "sha256-kg3dFbyEz2CjPPDGi7Zqje140VBGPBXrqifPt/OmwLc=";
   };
 
   # FIXME rename
   translations = fetchSrc {
     name = "translations";
-    sha256 = "sha256-yAU/hjyVwxqDoHm7Lu/Ztmb/1Z5AxDRAmMBKkkpU9uE=";
+    sha256 = "sha256-o/yee2qLPLyFPVvFLKysdzc37DOi+0wKYuCPrxspuRo=";
   };
 
   # the "dictionaries" archive is not used for LO build because we already build hunspellDicts packages from
@@ -31,6 +31,6 @@ rec {
 
   help = fetchSrc {
     name = "help";
-    sha256 = "sha256-T57V3Z2LOUvkQt24b1fLeHRigtiG4Nw1rdNuizQXD1w=";
+    sha256 = "sha256-6aiS/LdhY3nv3ockNmqEUPcVfOA7iJPjhUJdbzT3qLM=";
   };
 }
