7f8cbcecb789e60de2e7da0135834e53c2796f8c
Peter Hoeg <peter@hoeg.com>
2023-09-26 13:23:26 +0200

    nixos/minetest: run using DynamicUser

--- nixos/modules/services/games/minetest-server.nix
+++ nixos/modules/services/games/minetest-server.nix
@@ -1,8 +1,11 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStrings escapeShellArgs getExe getLib makeLibraryPath mapAttrsToList
+    mdDoc mkIf mkOption types
+    optionals;
+
   CONTAINS_NEWLINE_RE = ".*\n.*";
   # The following values are reserved as complete option values:
   # { - start of a group.
@@ -13,6 +16,8 @@ let
   # There is no way to encode """ on its own line in a Minetest config.
   UNESCAPABLE_RE = ".*\n\"\"\"\n.*";
 
+  dir = "/var/lib/minetest";
+
   toConfMultiline = name: value:
     assert lib.assertMsg
       ((builtins.match UNESCAPABLE_RE value) == null)
@@ -20,8 +25,8 @@ let
     "${name} = \"\"\"\n${value}\n\"\"\"\n";
 
   toConf = values:
-    lib.concatStrings
-      (lib.mapAttrsToList
+    concatStrings
+      (mapAttrsToList
         (name: value: {
           bool = "${name} = ${toString value}\n";
           int = "${name} = ${toString value}\n";
@@ -34,35 +39,43 @@ let
         }.${builtins.typeOf value})
         values);
 
-  cfg   = config.services.minetest-server;
-  flag  = val: name: lib.optionals (val != null) ["--${name}" "${toString val}"];
+  cfg = config.services.minetest-server;
 
-  flags = [
-    "--server"
-  ]
-    ++ (
-      if cfg.configPath != null
-      then ["--config" cfg.configPath]
-      else ["--config" (builtins.toFile "minetest.conf" (toConf cfg.config))])
+  flags =
+    let
+      flag = val: name:
+        optionals (val != null) [ "--${name}" "${toString val}" ];
+
+      file =
+        if cfg.configPath != null
+        then cfg.configPath
+        else (builtins.toFile "minetest.conf" (toConf cfg.config));
+    in
+    [
+      "--server"
+      "--config"
+      file
+    ]
     ++ (flag cfg.gameId "gameid")
     ++ (flag cfg.world "world")
     ++ (flag cfg.logPath "logfile")
     ++ (flag cfg.port "port")
     ++ cfg.extraArgs;
+
 in
 {
   options = {
     services.minetest-server = {
       enable = mkOption {
-        type        = types.bool;
-        default     = false;
-        description = lib.mdDoc "If enabled, starts a Minetest Server.";
+        type = types.bool;
+        default = false;
+        description = mdDoc "If enabled, starts a Minetest Server.";
       };
 
       gameId = mkOption {
-        type        = types.nullOr types.str;
-        default     = null;
-        description = lib.mdDoc ''
+        type = types.nullOr types.str;
+        default = null;
+        description = mdDoc ''
           Id of the game to use. To list available games run
           `minetestserver --gameid list`.
 
@@ -71,9 +84,9 @@ in
       };
 
       world = mkOption {
-        type        = types.nullOr types.path;
-        default     = null;
-        description = lib.mdDoc ''
+        type = types.nullOr types.path;
+        default = null;
+        description = mdDoc ''
           Name of the world to use. To list available worlds run
           `minetestserver --world list`.
 
@@ -82,9 +95,9 @@ in
       };
 
       configPath = mkOption {
-        type        = types.nullOr types.path;
-        default     = null;
-        description = lib.mdDoc ''
+        type = types.nullOr types.path;
+        default = null;
+        description = mdDoc ''
           Path to the config to use.
 
           If set to null, the config of the running user will be used:
@@ -94,8 +107,8 @@ in
 
       config = mkOption {
         type = types.attrsOf types.anything;
-        default = {};
-        description = lib.mdDoc ''
+        default = { };
+        description = mdDoc ''
           Settings to add to the minetest config file.
 
           This option is ignored if `configPath` is set.
@@ -103,9 +116,9 @@ in
       };
 
       logPath = mkOption {
-        type        = types.nullOr types.path;
-        default     = null;
-        description = lib.mdDoc ''
+        type = types.nullOr types.path;
+        default = null;
+        description = mdDoc ''
           Path to logfile for logging.
 
           If set to null, logging will be output to stdout which means
@@ -114,19 +127,17 @@ in
       };
 
       port = mkOption {
-        type        = types.nullOr types.int;
-        default     = null;
-        description = lib.mdDoc ''
+        type = types.port;
+        default = 30000;
+        description = mdDoc ''
           Port number to bind to.
-
-          If set to null, the default 30000 will be used.
         '';
       };
 
       extraArgs = mkOption {
         type = types.listOf types.str;
-        default = [];
-        description = lib.mdDoc ''
+        default = [ ];
+        description = mdDoc ''
           Additional command line flags to pass to the minetest executable.
         '';
       };
@@ -134,29 +145,25 @@ in
   };
 
   config = mkIf cfg.enable {
-    users.users.minetest = {
-      description     = "Minetest Server Service user";
-      home            = "/var/lib/minetest";
-      createHome      = true;
-      uid             = config.ids.uids.minetest;
-      group           = "minetest";
-    };
-    users.groups.minetest.gid = config.ids.gids.minetest;
-
     systemd.services.minetest-server = {
-      description   = "Minetest Server Service";
-      wantedBy      = [ "multi-user.target" ];
-      after         = [ "network.target" ];
-
-      serviceConfig.Restart = "always";
-      serviceConfig.User    = "minetest";
-      serviceConfig.Group   = "minetest";
-
-      script = ''
-        cd /var/lib/minetest
-
-        exec ${pkgs.minetest}/bin/minetest ${lib.escapeShellArgs flags}
-      '';
+      description = "Minetest Server Service";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      environment.HOME = dir;
+      serviceConfig = {
+        ExecStart = "${getExe pkgs.minetest} ${escapeShellArgs flags}";
+        DynamicUser = true;
+        StateDirectory = builtins.baseNameOf dir;
+        Restart = "always";
+        RestartSec = "5s";
+        User = "minetest";
+        Group = "minetest";
+        PrivateTmp = true;
+        ProtectHome = "tmpfs";
+        ProtectSystem = "strict";
+        SyslogIdentifier = "%N";
+        WorkingDirectory = dir;
+      };
     };
   };
 }
