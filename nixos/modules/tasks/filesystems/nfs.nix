7d6478a7cbd2b74274eeedd38d20445763538d45
Peter Hoeg <peter@hoeg.com>
2024-04-04 13:58:17 +0200

    nixos-24.05/nfs: RFC42 cleanup

--- nixos/modules/tasks/filesystems/nfs.nix
+++ nixos/modules/tasks/filesystems/nfs.nix
@@ -1,8 +1,9 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    optional optionalAttrs optionalString recursiveUpdate
+    mkDefault mkIf mkOption literalExpression types;
 
   inInitrd = config.boot.initrd.supportedFilesystems.nfs or false;
 
@@ -10,16 +11,17 @@ let
 
   rpcMountpoint = "${nfsStateDir}/rpc_pipefs";
 
-  format = pkgs.formats.ini {};
+  format = pkgs.formats.ini { };
 
   idmapdConfFile = format.generate "idmapd.conf" cfg.idmapd.settings;
 
   # merge parameters from services.nfs.server
   nfsConfSettings =
-    optionalAttrs (cfg.server.nproc != null) {
-      nfsd.threads = cfg.server.nproc;
-    } // optionalAttrs (cfg.server.hostName != null) {
-      nfsd.host= cfg.hostName;
+    optionalAttrs (cfg.server.nproc != null)
+      {
+        nfsd.threads = cfg.server.nproc;
+      } // optionalAttrs (cfg.server.hostName != null) {
+      nfsd.host = cfg.hostName;
     } // optionalAttrs (cfg.server.mountdPort != null) {
       mountd.port = cfg.server.mountdPort;
     } // optionalAttrs (cfg.server.statdPort != null) {
@@ -49,7 +51,7 @@ let
   '';
 
   nfsConfFile =
-    if cfg.settings != {}
+    if cfg.settings != { }
     then format.generate "nfs.conf" (recursiveUpdate nfsConfSettings cfg.settings)
     else pkgs.writeText "nfs.conf" nfsConfDeprecated;
 
@@ -64,51 +66,49 @@ in
 {
   ###### interface
 
-  options = {
-    services.nfs = {
-      idmapd.settings = mkOption {
-        type = format.type;
-        default = {};
-        description = ''
-          libnfsidmap configuration. Refer to
-          <https://linux.die.net/man/5/idmapd.conf>
-          for details.
-        '';
-        example = literalExpression ''
-          {
-            Translation = {
-              GSS-Methods = "static,nsswitch";
-            };
-            Static = {
-              "root/hostname.domain.com@REALM.COM" = "root";
-            };
-          }
-        '';
-      };
-      settings = mkOption {
-        type = format.type;
-        default = {};
-        description = ''
-          General configuration for NFS daemons and tools.
-          See nfs.conf(5) and related man pages for details.
-        '';
-        example = literalExpression ''
-          {
-            mountd.manage-gids = true;
-          }
-        '';
-      };
-      extraConfig = mkOption {
-        type = types.lines;
-        default = "";
-        description = ''
-          Extra nfs-utils configuration.
-        '';
-      };
+  options.services.nfs = {
+    idmapd.settings = mkOption {
+      type = format.type;
+      default = { };
+      description = ''
+        libnfsidmap configuration. Refer to
+        <https://linux.die.net/man/5/idmapd.conf>
+        for details.
+      '';
+      example = literalExpression ''
+        {
+          Translation = {
+            GSS-Methods = "static,nsswitch";
+          };
+          Static = {
+            "root/hostname.domain.com@REALM.COM" = "root";
+          };
+        }
+      '';
+    };
+
+    settings = mkOption {
+      type = format.type;
+      default = { };
+      description = ''
+        General configuration for NFS daemons and tools.
+        See nfs.conf(5) and related man pages for details.
+      '';
+      example = literalExpression ''
+        {
+          mountd.manage-gids = true;
+        }
+      '';
+    };
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = ''
+        Extra nfs-utils configuration.
+      '';
     };
   };
-
-  ###### implementation
 
   config = mkIf (config.boot.supportedFilesystems.nfs or config.boot.supportedFilesystems.nfs4 or false) {
 
@@ -119,80 +119,85 @@ in
         `services.nfs.server.extraNfsdConfig` is deprecated. Use `services.nfs.settings` instead.
       '');
     assertions = [{
-      assertion = cfg.settings != {} -> cfg.extraConfig == "" && cfg.server.extraNfsdConfig == "";
+      assertion = cfg.settings != { } -> cfg.extraConfig == "" && cfg.server.extraNfsdConfig == "";
       message = "`services.nfs.settings` cannot be used together with `services.nfs.extraConfig` and `services.nfs.server.extraNfsdConfig`.";
     }];
 
-    services.rpcbind.enable = true;
-
-    services.nfs.idmapd.settings = {
-      General = mkMerge [
-        { Pipefs-Directory = rpcMountpoint; }
-        (mkIf (config.networking.domain != null) { Domain = config.networking.domain; })
-      ];
-      Mapping = {
-        Nobody-User = "nobody";
-        Nobody-Group = "nogroup";
+    services = {
+      nfs.idmapd.settings = {
+        General = {
+          Pipefs-Directory = rpcMountpoint;
+        }
+        // optionalAttrs (config.networking.domain != null) { Domain = config.networking.domain; };
+        Mapping = {
+          Nobody-User = "nobody";
+          Nobody-Group = "nogroup";
+        };
+        Translation = {
+          Method = "nsswitch";
+        };
       };
-      Translation = {
-        Method = "nsswitch";
-      };
+      rpcbind.enable = true;
     };
 
     system.fsPackages = [ pkgs.nfs-utils ];
 
     boot.initrd.kernelModules = mkIf inInitrd [ "nfs" ];
 
-    systemd.packages = [ pkgs.nfs-utils ];
-
-    environment.systemPackages = [ pkgs.keyutils ];
-
-    environment.etc = {
-      "idmapd.conf".source = idmapdConfFile;
-      "nfs.conf".source = nfsConfFile;
-      "request-key.conf".source = requestKeyConfFile;
+    environment = {
+      etc = {
+        "idmapd.conf".source = idmapdConfFile;
+        "nfs.conf".source = nfsConfFile;
+        "request-key.conf".source = requestKeyConfFile;
+      };
+      systemPackages = [ pkgs.keyutils ];
     };
 
-    systemd.services.nfs-blkmap =
-      { restartTriggers = [ nfsConfFile ];
+    systemd = {
+      packages = [ pkgs.nfs-utils ];
+
+      services = {
+        nfs-blkmap = {
+          restartTriggers = [ nfsConfFile ];
+        };
+
+        nfs-idmapd = {
+          restartTriggers = [ idmapdConfFile ];
+        };
+
+        nfs-mountd = {
+          restartTriggers = [ nfsConfFile ];
+          enable = mkDefault false;
+        };
+
+        nfs-server = {
+          restartTriggers = [ nfsConfFile ];
+          enable = mkDefault false;
+        };
+
+        auth-rpcgss-module = {
+          unitConfig.ConditionPathExists = [ "" "/etc/krb5.keytab" ];
+        };
+
+        rpc-gssd = {
+          restartTriggers = [ nfsConfFile ];
+          unitConfig.ConditionPathExists = [ "" "/etc/krb5.keytab" ];
+        };
+
+        rpc-statd = {
+          restartTriggers = [ nfsConfFile ];
+        };
       };
 
-    systemd.targets.nfs-client =
-      { wantedBy = [ "multi-user.target" "remote-fs.target" ];
+      targets.nfs-client = {
+        wantedBy = [ "multi-user.target" "remote-fs.target" ];
       };
 
-    systemd.services.nfs-idmapd =
-      { restartTriggers = [ idmapdConfFile ];
-      };
-
-    systemd.services.nfs-mountd =
-      { restartTriggers = [ nfsConfFile ];
-        enable = mkDefault false;
-      };
-
-    systemd.services.nfs-server =
-      { restartTriggers = [ nfsConfFile ];
-        enable = mkDefault false;
-      };
-
-    systemd.services.auth-rpcgss-module =
-      {
-        unitConfig.ConditionPathExists = [ "" "/etc/krb5.keytab" ];
-      };
-
-    systemd.services.rpc-gssd =
-      { restartTriggers = [ nfsConfFile ];
-        unitConfig.ConditionPathExists = [ "" "/etc/krb5.keytab" ];
-      };
-
-    systemd.services.rpc-statd =
-      { restartTriggers = [ nfsConfFile ];
-
-        preStart =
-          ''
-            mkdir -p /var/lib/nfs/{sm,sm.bak}
-          '';
-      };
-
+      tmpfiles.rules = [
+        "d ${nfsStateDir}/sm     0700 nobody root - -"
+        "d ${nfsStateDir}/sm.bak 0700 nobody root - -"
+        "d ${rpcMountpoint}      0555 - - - -"
+      ];
+    };
   };
 }
