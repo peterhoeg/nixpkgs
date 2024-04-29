29b6522e1ad09e9c9834200fb397ba989b7d55b1
Martino Fontana <tinozzo123@gmail.com>
2024-04-29 17:09:44 +0200

    pcsx2: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2714,8 +2714,6 @@ with pkgs;
 
   pcem = callPackage ../applications/emulators/pcem { };
 
-  pcsx2 = qt6Packages.callPackage ../applications/emulators/pcsx2 { };
-
   pcsxr = callPackage ../applications/emulators/pcsxr { };
 
   ppsspp-sdl = let
