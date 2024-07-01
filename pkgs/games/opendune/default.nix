dfcc9976cbeb92e5b3a0381dcc765463764c594f
Peter Hoeg <peter@hoeg.com>
2024-07-01 20:19:22 +0200

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_opendune_git.patch

--- pkgs/games/opendune/default.nix
+++ pkgs/games/opendune/default.nix
@@ -1,30 +1,66 @@
-{ stdenv, lib, fetchFromGitHub, pkg-config
-, alsa-lib, libpulseaudio, SDL2, SDL2_image, SDL2_mixer }:
+{ stdenv
+, lib
+, fetchFromGitHub
+, gawk
+, pkg-config
+, writeScriptBin
+, alsa-lib
+, libpulseaudio
+, SDL2
+, SDL2_image
+}:
 
 # - set the opendune configuration at ~/.config/opendune/opendune.ini:
 #     [opendune]
 #     datadir=/path/to/opendune-data
-# - download dune2 into [datadir] http://www.bestoldgames.net/eng/old-games/dune-2.php
+# - download dune2 into [datadir] https://www.bestoldgames.net/eng/old-games/dune-2.php
 
-stdenv.mkDerivation rec {
+let
+  inherit (lib) concatStringsSep getExe getLib;
+
+  rev = "0626d4f533798c667e365955fc2d7a50b94f97cd";
+
+  # https://github.com/OpenDUNE/OpenDUNE/blob/master/findversion.sh
+  vers = writeScriptBin "findversion.sh" ''
+    echo -e "${concatStringsSep "\\t" (map toString [ "" "" 0 rev ])}"
+  '';
+
+in
+stdenv.mkDerivation (_finalAttrs: {
   pname = "opendune";
-  version = "0.9";
+  version = "0.9.20210826";
 
   src = fetchFromGitHub {
     owner = "OpenDUNE";
     repo = "OpenDUNE";
-    rev = version;
-    sha256 = "15rvrnszdy3db8s0dmb696l4isb3x2cpj7wcl4j09pdi59pc8p37";
+    inherit rev;
+    hash = "sha256-ObptehpICsRwE8LgaVnHh+wBOAds7y4eTsOnfBjSznI=";
   };
 
+  postPatch = ''
+    # to avoid sleeping during the configure phase
+    touch .ottdrev
+    # ensure we can detect the proper version
+    ln -sf ${getExe vers} ./findversion.sh
+  '';
+
   configureFlags = [
-    "--with-alsa=${lib.getLib alsa-lib}/lib/libasound.so"
-    "--with-pulse=${lib.getLib libpulseaudio}/lib/libpulse.so"
+    "--enable-lto"
+    "--with-alsa=${getLib alsa-lib}/lib/libasound.so"
+    "--with-pulse=${getLib libpulseaudio}/lib/libpulse.so"
   ];
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [ gawk pkg-config ];
 
-  buildInputs = [ alsa-lib libpulseaudio SDL2 SDL2_image SDL2_mixer ];
+  buildInputs = [ alsa-lib libpulseaudio SDL2 SDL2_image ];
+
+  env.NIX_CFLAGS_COMPILE = concatStringsSep " " [
+    "-Wno-format-overflow"
+    "-Wno-redundant-decls"
+    "-Wno-stringop-truncation"
+    "-Wno-unused-but-set-variable"
+    "-Wno-unused-function"
+  ];
 
   enableParallelBuilding = true;
 
@@ -32,7 +68,7 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     install -Dm555 -t $out/bin bin/opendune
-    install -Dm444 -t $out/share/doc/opendune enhancement.txt README.txt
+    install -Dm444 -t $out/share/doc/opendune *.txt
 
     runHook postInstall
   '';
@@ -41,7 +77,7 @@ stdenv.mkDerivation rec {
     description = "Dune, Reinvented";
     mainProgram = "opendune";
     homepage = "https://github.com/OpenDUNE/OpenDUNE";
-    license = licenses.gpl2;
-    maintainers = with maintainers; [ ];
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [ peterhoeg ];
   };
-}
+})
