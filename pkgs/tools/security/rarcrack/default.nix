15ef504691b599071b3f08081fa4b630693db102
Peter Hoeg <peter@hoeg.com>
2024-10-19 10:17:07 +0200

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_rarcrack.patch

--- pkgs/tools/security/rarcrack/default.nix
+++ pkgs/tools/security/rarcrack/default.nix
@@ -1,23 +1,27 @@
-{lib, stdenv, fetchFromGitHub, libxml2, file, p7zip, unrar, unzip}:
+{ lib, stdenv, fetchFromGitHub, libxml2, file, p7zip, unrar, unzip }:
 
 stdenv.mkDerivation {
   pname = "rarcrack";
-  version = "0.2";
+  version = "0.2.20231004";
 
   src = fetchFromGitHub {
     owner = "jaredsburrows";
     repo = "Rarcrack";
-    rev = "35ead64cd2b967eec3e3e3a4c328b89b11ff32a0";
-    sha256 = "134fq84896w5vp8vg4qg0ybpb466njibigyd7bqqm1xydr07qrgn";
+    rev = "278b34c99f486feecfc654abb3af64ac394cec35";
+    hash = "sha256-6q66OKpb1RnDiV05V3OJ/MxgW8/rQ7SIaSF+JfN8jM0=";
   };
 
   nativeBuildInputs = [ unzip ];
-  buildInputs = [ libxml2 file p7zip unrar ];
-  buildFlags = lib.optional stdenv.cc.isClang "CC=clang";
-  installFlags = [ "PREFIX=\${out}" ];
 
-  patchPhase = ''
-   substituteInPlace rarcrack.c --replace "file -i" "${file}/bin/file -i"
+  buildInputs = [ libxml2 file p7zip unrar ];
+
+  buildFlags = lib.optional stdenv.cc.isClang "CC=clang";
+
+  installFlags = [ "PREFIX=${placeholder "out"}" ];
+
+  postPatch = ''
+    substituteInPlace rarcrack.c \
+      --replace "file -i" "${file}/bin/file -i"
   '';
 
   preInstall = ''
@@ -27,9 +31,9 @@ stdenv.mkDerivation {
   meta = with lib; {
     description = "This program can crack zip,7z and rar file passwords";
     longDescription = ''
-    If you forget your password for compressed archive (rar, 7z, zip), this program is the solution.
-    This program uses bruteforce algorithm to find correct password. You can specify wich characters will be used in password generations.
-    Warning: Please don't use this program for any illegal things!
+      If you forget your password for compressed archive (rar, 7z, zip), this program is the solution.
+      This program uses bruteforce algorithm to find correct password. You can specify wich characters will be used in password generations.
+      Warning: Please don't use this program for any illegal things!
     '';
     homepage = "https://github.com/jaredsburrows/Rarcrack";
     license = licenses.gpl2Only;
