34c2b923124d1717f839c39090d5ac252577b0b3
Peter Hoeg <peter@hoeg.com>
2024-12-10 10:00:20 +0100

    auto commit of patch: /home/peter/src/nix/nixos/patches/24_05_minetest_module.patch

--- nixos/modules/services/games/minetest-server.nix
+++ nixos/modules/services/games/minetest-server.nix
@@ -1,8 +1,19 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOption
+    types
+    ;
+
   CONTAINS_NEWLINE_RE = ".*\n.*";
   # The following values are reserved as complete option values:
   # { - start of a group.
@@ -13,55 +24,123 @@ let
   # There is no way to encode """ on its own line in a Minetest config.
   UNESCAPABLE_RE = ".*\n\"\"\"\n.*";
 
-  toConfMultiline = name: value:
-    assert lib.assertMsg
-      ((builtins.match UNESCAPABLE_RE value) == null)
-      ''""" can't be on its own line in a minetest config.'';
+  dir = "/var/lib/minetest";
+
+  toConfMultiline =
+    name: value:
+    assert lib.assertMsg (
+      (builtins.match UNESCAPABLE_RE value) == null
+    ) ''""" can't be on its own line in a minetest config.'';
     "${name} = \"\"\"\n${value}\n\"\"\"\n";
 
-  toConf = values:
-    lib.concatStrings
-      (lib.mapAttrsToList
-        (name: value: {
+  toConf =
+    values:
+    lib.concatStrings (
+      lib.mapAttrsToList (
+        name: value:
+        {
           bool = "${name} = ${toString value}\n";
           int = "${name} = ${toString value}\n";
           null = "";
           set = "${name} = {\n${toConf value}}\n";
           string =
-            if (builtins.match NEEDS_MULTILINE_RE value) != null
-            then toConfMultiline name value
-            else "${name} = ${value}\n";
-        }.${builtins.typeOf value})
-        values);
+            if (builtins.match NEEDS_MULTILINE_RE value) != null then
+              toConfMultiline name value
+            else
+              "${name} = ${value}\n";
+        }
+        .${builtins.typeOf value}
+      ) values
+    );
 
-  cfg   = config.services.minetest-server;
-  flag  = val: name: lib.optionals (val != null) ["--${name}" "${toString val}"];
+  cfg = config.services.minetest-server;
 
-  flags = [
-    "--server"
-  ]
-    ++ (
-      if cfg.configPath != null
-      then ["--config" cfg.configPath]
-      else ["--config" (builtins.toFile "minetest.conf" (toConf cfg.config))])
+  flags =
+    let
+      flag =
+        val: name:
+        lib.optionals (val != null) [
+          "--${name}"
+          "${toString val}"
+        ];
+
+      file =
+        if cfg.configPath != null then
+          cfg.configPath
+        else
+          (builtins.toFile "minetest.conf" (toConf cfg.config));
+    in
+    [
+      "--config"
+      file
+    ]
     ++ (flag cfg.gameId "gameid")
     ++ (flag cfg.world "world")
     ++ (flag cfg.logPath "logfile")
     ++ (flag cfg.port "port")
     ++ cfg.extraArgs;
+
+  addonsSetup =
+    let
+      collection = pkgs.symlinkJoin {
+        name = "luanti-addons";
+        paths = cfg.addons;
+      };
+    in
+    pkgs.resholve.writeScriptBin "minetest-addons-setup"
+      {
+        interpreter = pkgs.runtimeShell;
+        inputs = with pkgs; [
+          coreutils
+          findutils
+        ];
+        execer = map (e: "cannot:${if builtins.isString e then e else lib.getExe e}") [ ];
+      }
+      ''
+        set -eEuo pipefail
+
+        _copy() {
+          local kind; kind="$1"
+          local tgt=${dir}/.minetest/$kind
+
+          test -d $tgt || mkdir -p $tgt
+
+          for d in ${collection}/share/minetest/$kind/*; do
+            base="$(basename "$d")"
+            rm -rf "$tgt/$base"
+            cp -R --dereference --no-preserve=all $d $tgt/
+          done
+        }
+
+        _link() {
+          local kind; kind="$1"
+          local tgt=${dir}/.minetest/$kind
+
+          test -d $tgt || mkdir -p $tgt
+
+          find $tgt -maxdepth 1 -type l -delete
+          ln -s ${collection}/share/minetest/$kind/* -t $tgt
+        }
+
+        for kind in games mods textures; do
+          # we cannot symlink as that runs afoul of minetest's secure environment, but need to copy
+          # the files into place
+
+          _copy $kind
+        done
+      '';
+
 in
 {
   options = {
     services.minetest-server = {
-      enable = mkOption {
-        type        = types.bool;
-        default     = false;
-        description = "If enabled, starts a Minetest Server.";
-      };
+      enable = mkEnableOption "start a Minetest Server.";
+
+      package = mkPackageOption pkgs "minetestserver" { };
 
       gameId = mkOption {
-        type        = types.nullOr types.str;
-        default     = null;
+        type = types.nullOr types.str;
+        default = null;
         description = ''
           Id of the game to use. To list available games run
           `minetestserver --gameid list`.
@@ -71,8 +150,13 @@ in
       };
 
       world = mkOption {
-        type        = types.nullOr types.path;
-        default     = null;
+        type = types.nullOr (
+          types.oneOf [
+            types.path
+            types.str
+          ]
+        );
+        default = null;
         description = ''
           Name of the world to use. To list available worlds run
           `minetestserver --world list`.
@@ -82,8 +166,8 @@ in
       };
 
       configPath = mkOption {
-        type        = types.nullOr types.path;
-        default     = null;
+        type = types.nullOr types.path;
+        default = null;
         description = ''
           Path to the config to use.
 
@@ -94,7 +178,7 @@ in
 
       config = mkOption {
         type = types.attrsOf types.anything;
-        default = {};
+        default = { };
         description = ''
           Settings to add to the minetest config file.
 
@@ -103,8 +187,8 @@ in
       };
 
       logPath = mkOption {
-        type        = types.nullOr types.path;
-        default     = null;
+        type = types.nullOr types.path;
+        default = null;
         description = ''
           Path to logfile for logging.
 
@@ -114,18 +198,22 @@ in
       };
 
       port = mkOption {
-        type        = types.nullOr types.int;
-        default     = null;
+        type = types.port;
+        default = 30000;
         description = ''
           Port number to bind to.
-
-          If set to null, the default 30000 will be used.
         '';
+      };
+
+      addons = mkOption {
+        type = types.listOf types.package;
+        description = "Addons";
+        default = [ ];
       };
 
       extraArgs = mkOption {
         type = types.listOf types.str;
-        default = [];
+        default = [ ];
         description = ''
           Additional command line flags to pass to the minetest executable.
         '';
@@ -134,29 +222,26 @@ in
   };
 
   config = mkIf cfg.enable {
-    users.users.minetest = {
-      description     = "Minetest Server Service user";
-      home            = "/var/lib/minetest";
-      createHome      = true;
-      uid             = config.ids.uids.minetest;
-      group           = "minetest";
-    };
-    users.groups.minetest.gid = config.ids.gids.minetest;
-
     systemd.services.minetest-server = {
-      description   = "Minetest Server Service";
-      wantedBy      = [ "multi-user.target" ];
-      after         = [ "network.target" ];
-
-      serviceConfig.Restart = "always";
-      serviceConfig.User    = "minetest";
-      serviceConfig.Group   = "minetest";
-
-      script = ''
-        cd /var/lib/minetest
-
-        exec ${pkgs.minetest}/bin/minetest ${lib.escapeShellArgs flags}
-      '';
+      description = "Minetest Server Service";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      environment.HOME = dir;
+      serviceConfig = {
+        ExecStartPre = lib.getExe addonsSetup;
+        ExecStart = "${lib.getExe cfg.package} ${lib.escapeShellArgs flags}";
+        StateDirectory = builtins.baseNameOf dir;
+        Restart = "always";
+        RestartSec = "5s";
+        DynamicUser = true;
+        User = "minetest";
+        Group = "minetest";
+        PrivateTmp = true;
+        ProtectHome = "tmpfs";
+        ProtectSystem = "strict";
+        SyslogIdentifier = "%N";
+        WorkingDirectory = dir;
+      };
     };
   };
 }
