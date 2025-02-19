c6bd76d7d75107a48eb67ff6d85c715321de680d
Peter H. Hoeg <peter@hoeg.com>
2025-02-19 16:40:20 +0100

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_networkmanager_openconnect.patch

--- nixos/modules/services/networking/networkmanager.nix
+++ nixos/modules/services/networking/networkmanager.nix
@@ -664,10 +664,10 @@ in
         networkmanager.plugins = with pkgs; [
           networkmanager-fortisslvpn
           networkmanager-iodine
-          networkmanager-l2tp
-          networkmanager-openconnect
+          # networkmanager-l2tp
+          # networkmanager-openconnect
           networkmanager-openvpn
-          networkmanager-vpnc
+          # networkmanager-vpnc
           networkmanager-sstp
         ];
       }
