03e3a047f5e90bcf4fe2d734035d0441d72ef4ba
Peter H. Hoeg <peter@hoeg.com>
2025-05-21 13:10:08 +0200

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_llama.patch

--- pkgs/by-name/ll/llama-cpp/package.nix
+++ pkgs/by-name/ll/llama-cpp/package.nix
@@ -72,13 +72,13 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "llama-cpp";
-  version = "5416";
+  version = "5440";
 
   src = fetchFromGitHub {
     owner = "ggml-org";
     repo = "llama.cpp";
     tag = "b${finalAttrs.version}";
-    hash = "sha256-/UFfHPGJRPnLEA9JninLazrwO3koE5AaK4oHK7yoN+0=";
+    hash = "sha256-Knq46BA3IpqQarv8h6SiFdrGMNQnFsD27M0L4BbXIzg=";
     leaveDotGit = true;
     postFetch = ''
       git -C "$out" rev-parse --short HEAD > $out/COMMIT
