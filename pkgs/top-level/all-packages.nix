827abd220692f43b01604e0ad841b90c64d0d5ee
Peter H. Hoeg <peter@hoeg.com>
2025-07-31 14:06:06 +0200

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_heimer.patch

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12450,8 +12450,6 @@ with pkgs;
 
   hachoir = with python3Packages; toPythonApplication hachoir;
 
-  heimer = libsForQt5.callPackage ../applications/misc/heimer { };
-
   hydrogen-web-unwrapped =
     callPackage ../applications/networking/instant-messengers/hydrogen-web/unwrapped.nix
       { };
