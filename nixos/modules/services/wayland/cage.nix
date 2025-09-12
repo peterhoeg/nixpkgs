6cdbed2fc7ea3478da08a429dacb71b02efff056
Peter H. Hoeg <peter@hoeg.com>
2025-09-12 14:20:14 +0200

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_cage.patch

--- nixos/modules/services/wayland/cage.nix
+++ nixos/modules/services/wayland/cage.nix
@@ -5,102 +5,117 @@
   ...
 }:
 
-with lib;
-
 let
   cfg = config.services.cage;
 in
 {
-  options.services.cage.enable = mkEnableOption "cage kiosk service";
+  options.services.cage = with lib.types; {
+    enable = lib.mkEnableOption "cage kiosk service";
 
-  options.services.cage.user = mkOption {
-    type = types.str;
-    default = "demo";
-    description = ''
-      User to log-in as.
-    '';
-  };
-
-  options.services.cage.extraArguments = mkOption {
-    type = types.listOf types.str;
-    default = [ ];
-    defaultText = literalExpression "[]";
-    description = "Additional command line arguments to pass to Cage.";
-    example = [ "-d" ];
-  };
-
-  options.services.cage.environment = mkOption {
-    type = types.attrsOf types.str;
-    default = { };
-    example = {
-      WLR_LIBINPUT_NO_DEVICES = "1";
+    tty = lib.mkOption {
+      type = types.str;
+      default = "tty1";
+      description = ''
+        The TTY on which to run.
+      '';
     };
-    description = "Additional environment variables to pass to Cage.";
+
+    user = lib.mkOption {
+      type = types.str;
+      default = "demo";
+      description = ''
+        User to log-in as.
+      '';
+    };
+
+    extraArguments = lib.mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      defaultText = lib.literalExpression "[]";
+      description = "Additional command line arguments to pass to Cage.";
+      example = [ "-d" ];
+    };
+
+    environment = lib.mkOption {
+      type = types.attrsOf types.str;
+      default = { };
+      example = {
+        WLR_LIBINPUT_NO_DEVICES = "1";
+      };
+      description = "Additional environment variables to pass to Cage.";
+    };
+
+    program = lib.mkOption {
+      type = types.path;
+      default = "${pkgs.xterm}/bin/xterm";
+      defaultText = lib.literalExpression ''"''${pkgs.xterm}/bin/xterm"'';
+      description = ''
+        Program to run in cage.
+      '';
+    };
+
+    package = lib.mkPackageOption pkgs "cage" { };
   };
 
-  options.services.cage.program = mkOption {
-    type = types.path;
-    default = "${pkgs.xterm}/bin/xterm";
-    defaultText = literalExpression ''"''${pkgs.xterm}/bin/xterm"'';
-    description = ''
-      Program to run in cage.
-    '';
-  };
-
-  options.services.cage.package = mkPackageOption pkgs "cage" { };
-
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
 
     # The service is partially based off of the one provided in the
     # cage wiki at
     # https://github.com/Hjdskes/cage/wiki/Starting-Cage-on-boot-with-systemd.
-    systemd.services."cage-tty1" = {
-      enable = true;
-      after = [
-        "systemd-user-sessions.service"
-        "plymouth-start.service"
-        "plymouth-quit.service"
-        "systemd-logind.service"
-        "getty@tty1.service"
-      ];
-      before = [ "graphical.target" ];
-      wants = [
-        "dbus.socket"
-        "systemd-logind.service"
-        "plymouth-quit.service"
-      ];
-      wantedBy = [ "graphical.target" ];
-      conflicts = [ "getty@tty1.service" ];
+    systemd = {
+      services."cage-${cfg.tty}" = {
+        enable = true;
+        after = [
+          "systemd-user-sessions.service"
+          "plymouth-start.service"
+          "plymouth-quit.service"
+          "systemd-logind.service"
+          "getty@${cfg.tty}.service"
+        ];
+        before = [ "graphical.target" ];
+        wants = [
+          "dbus.socket"
+          "systemd-logind.service"
+          "plymouth-quit.service"
+        ];
+        wantedBy = [ "graphical.target" ];
+        conflicts = [ "getty@${cfg.tty}.service" ];
 
-      restartIfChanged = false;
-      unitConfig.ConditionPathExists = "/dev/tty1";
-      serviceConfig = {
-        ExecStart = ''
-          ${cfg.package}/bin/cage \
-            ${escapeShellArgs cfg.extraArguments} \
-            -- ${cfg.program}
-        '';
-        User = cfg.user;
+        restartIfChanged = false;
+        unitConfig.ConditionPathExists = "/dev/${cfg.tty}";
+        serviceConfig = {
+          ExecStart = lib.concatStringsSep " " [
+            (lib.getExe cfg.package)
+            (lib.escapeShellArgs cfg.extraArguments)
+            "--"
+            cfg.program
+          ];
+          User = cfg.user;
 
-        IgnoreSIGPIPE = "no";
+          IgnoreSIGPIPE = "no";
 
-        # Log this user with utmp, letting it show up with commands 'w' and
-        # 'who'. This is needed since we replace (a)getty.
-        UtmpIdentifier = "%n";
-        UtmpMode = "user";
-        # A virtual terminal is needed.
-        TTYPath = "/dev/tty1";
-        TTYReset = "yes";
-        TTYVHangup = "yes";
-        TTYVTDisallocate = "yes";
-        # Fail to start if not controlling the virtual terminal.
-        StandardInput = "tty-fail";
-        StandardOutput = "journal";
-        StandardError = "journal";
-        # Set up a full (custom) user session for the user, required by Cage.
-        PAMName = "cage";
+          # Log this user with utmp, letting it show up with commands 'w' and
+          # 'who'. This is needed since we replace (a)getty.
+          UtmpIdentifier = "%n";
+          UtmpMode = "user";
+          # A virtual terminal is needed.
+          TTYPath = "/dev/${cfg.tty}";
+          TTYReset = "yes";
+          TTYVHangup = "yes";
+          TTYVTDisallocate = "yes";
+          # Fail to start if not controlling the virtual terminal.
+          StandardInput = "tty-fail";
+          StandardOutput = "journal";
+          StandardError = "journal";
+          # Set up a full (custom) user session for the user, required by Cage.
+          PAMName = "cage";
+        };
+        inherit (cfg) environment;
       };
-      environment = cfg.environment;
+
+      targets.graphical.wants = [ "cage-tty1.service" ];
+
+      defaultUnit = "graphical.target";
     };
 
     security.polkit.enable = true;
@@ -113,13 +128,8 @@ in
       session required ${config.systemd.package}/lib/security/pam_systemd.so
     '';
 
-    hardware.graphics.enable = mkDefault true;
-
-    systemd.targets.graphical.wants = [ "cage-tty1.service" ];
-
-    systemd.defaultUnit = "graphical.target";
+    hardware.graphics.enable = lib.mkDefault true;
   };
 
   meta.maintainers = with lib.maintainers; [ matthewbauer ];
-
 }
