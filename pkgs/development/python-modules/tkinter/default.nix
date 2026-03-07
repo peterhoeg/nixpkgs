284c4c58aaf3cc6662653ac8231bb2215da12f4e
Peter H. Hoeg <peter@hoeg.com>
2026-03-07 17:11:39 +0100

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_tkinter.patch

--- pkgs/development/python-modules/tkinter/default.nix
+++ pkgs/development/python-modules/tkinter/default.nix
@@ -11,8 +11,13 @@
   tclPackages,
   tk,
   xvfb-run,
+  libtommath,
 }:
 
+let
+  py311 = python.pythonOlder "3.12";
+
+in
 buildPythonPackage {
   pname = "tkinter";
   version = python.version;
@@ -55,7 +60,8 @@ buildPythonPackage {
   buildInputs = [
     tcl
     tk
-  ];
+  ]
+  ++ lib.optionals py311 [ libtommath ];
 
   env = {
     TCLTK_LIBS = toString [
@@ -68,6 +74,9 @@ buildPythonPackage {
       "-I${lib.getDev tcl}/include"
       "-I${lib.getDev tk}/include"
     ];
+  }
+  // lib.optionalAttrs py311 {
+    NIX_CFLAGS_COMPILE = "-Wno-error=incompatible-pointer-types";
   };
 
   nativeCheckInputs = lib.optional stdenv.hostPlatform.isLinux xvfb-run;
