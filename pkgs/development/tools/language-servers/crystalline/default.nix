dd665a8721e65d93d70ff24405dec6969bdd3f72
Peter H. Hoeg <peter@hoeg.com>
2026-03-07 17:11:35 +0100

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_crystalline.patch

--- pkgs/development/tools/language-servers/crystalline/default.nix
+++ pkgs/development/tools/language-servers/crystalline/default.nix
@@ -4,13 +4,22 @@
   fetchFromGitHub,
   llvmPackages,
   openssl,
+  shards,
   makeWrapper,
+  _experimental-update-script-combinators,
+  crystal2nix,
+  runCommand,
+  writeShellScript,
+  gitUpdater,
+  testers,
+  crystalline,
 }:
 
 let
-  version = "0.15.0";
+  version = "0.17.1";
+
 in
-crystal.buildCrystalPackage {
+crystal.buildCrystalPackage rec {
   pname = "crystalline";
   inherit version;
 
@@ -18,7 +27,7 @@ crystal.buildCrystalPackage {
     owner = "elbywan";
     repo = "crystalline";
     rev = "v${version}";
-    hash = "sha256-6ZAogEuOJH1QQ6NSJ+8KZUSFSgQAcvd4U9vWNAGix/M=";
+    hash = "sha256-SIfInDY6KhEwEPZckgobOrpKXBDDd0KhQt/IjdGBhWo=";
   };
 
   format = "crystal";
@@ -28,7 +37,14 @@ crystal.buildCrystalPackage {
     llvmPackages.llvm
     openssl
     makeWrapper
+    shards
   ];
+
+  # for some reason just using env.LLVM_CONFIG isn't enough for the builder to find it, so export it manually early
+  preBuild = ''
+    export LLVM_CONFIG=${env.LLVM_CONFIG}
+  '';
+
   env.LLVM_CONFIG = lib.getExe' (lib.getDev llvmPackages.llvm) "llvm-config";
 
   doCheck = false;
@@ -45,12 +61,41 @@ crystal.buildCrystalPackage {
   };
 
   postInstall = ''
-    wrapProgram "$out/bin/crystalline" --prefix PATH : '${
+    wrapProgram "$out/bin/${meta.mainProgram}" --prefix PATH : '${
       lib.makeBinPath [
         (lib.getDev llvmPackages.llvm)
       ]
     }'
   '';
+
+  passthru = {
+    updateScript = _experimental-update-script-combinators.sequence [
+      (gitUpdater { rev-prefix = "v"; })
+      (_experimental-update-script-combinators.copyAttrOutputToFile "crystalline.shardLock" "${builtins.toString ./.}/shard.lock")
+      {
+        command = [
+          (writeShellScript "update-lock" "cd $1; ${lib.getExe crystal2nix}")
+          ./.
+        ];
+        supportedFeatures = [ "silent" ];
+      }
+      {
+        command = [
+          "rm"
+          "${builtins.toString ./.}/shard.lock"
+        ];
+        supportedFeatures = [ "silent" ];
+      }
+    ];
+    shardLock = runCommand "shard.lock" { inherit src; } ''
+      cp $src/shard.lock $out
+    '';
+
+    # Since doInstallCheck causes another test error, versionCheckHook is avoided.
+    tests.version = testers.testVersion {
+      package = crystalline;
+    };
+  };
 
   meta = {
     description = "Language Server Protocol implementation for Crystal";
