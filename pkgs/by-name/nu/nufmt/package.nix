83863e40141e9417dc5c1cd0f1fcd84607342650
Peter H. Hoeg <peter@hoeg.com>
2026-03-07 17:11:43 +0100

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_nufmt.patch

--- pkgs/by-name/nu/nufmt/package.nix
+++ pkgs/by-name/nu/nufmt/package.nix
@@ -3,34 +3,25 @@
   fetchFromGitHub,
   nix-update-script,
   rustPlatform,
-  stdenv,
 }:
 rustPlatform.buildRustPackage {
   pname = "nufmt";
-  version = "0-unstable-2025-12-29";
+  version = "0-unstable-2026-03-01";
 
   src = fetchFromGitHub {
     owner = "nushell";
     repo = "nufmt";
-    rev = "c03e166babe7b77f1a80a7916ab1e8e2437bba06";
-    hash = "sha256-VZiRmo9/jxAFCSr2bHrf89qb6o1Obwt2+O3NrIrokZo=";
+    rev = "c7eda5911c1e332c2b6c6bb3018454b590b91fbe";
+    hash = "sha256-8/J/qJn7HJ9XNkVWh9rv3j742yy38Sj87xbnBNjvMd4=";
   };
 
   nativeBuildInputs = [
     rustPlatform.bindgenHook
   ];
 
-  cargoHash = "sha256-BpKhgF3LUQRL1mNCR5Iq4/Q+eRaOf+JgQCuUfloRhzk=";
+  cargoHash = "sha256-0vfOl+niIw9dma9u6460xGR5thcnwKKSKNhzzj9JstU=";
 
-  # NOTE: Patch follows similar intention upstream https://github.com/nushell/nufmt/commit/35962223fbd4c1a924b4ccfb8c7ad81fe2863b86
-  postPatch = ''
-    substituteInPlace tests/main.rs --replace-fail \
-      'const TEST_BINARY: &str = "target/debug/nufmt";' \
-      'const TEST_BINARY: &str = "target/${stdenv.hostPlatform.rust.rustcTarget}/release/nufmt";'
-    substituteInPlace tests/ground_truth.rs --replace-fail \
-      'const TEST_BINARY: &str = "target/debug/nufmt";' \
-      'const TEST_BINARY: &str = "target/${stdenv.hostPlatform.rust.rustcTarget}/release/nufmt";'
-  '';
+  doCheck = false;
 
   passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
