709980102ff40e25ebc816356a90384afab28fb7
Peter H. Hoeg <peter@hoeg.com>
2026-09-25 11:18:09 +0200

    auto commit of patch: /home/peter/src/nix/nixos/patches/unstable_dbus_format.patch

--- pkgs/by-name/ma/makeDBusConf/package.nix
+++ pkgs/by-name/ma/makeDBusConf/package.nix
@@ -1,5 +1,6 @@
 {
   runCommand,
+  libxml2,
   libxslt,
   dbus,
   findXMLCatalogs,
@@ -21,6 +22,7 @@ runCommand "dbus-1"
     allowSubstitutes = false;
 
     nativeBuildInputs = [
+      libxml2.bin
       libxslt.bin
       findXMLCatalogs
     ];
@@ -30,6 +32,8 @@ runCommand "dbus-1"
     ];
   }
   ''
+    set -eo pipefail
+
     mkdir -p $out
 
     xsltproc --nonet \
@@ -37,12 +41,12 @@ runCommand "dbus-1"
       --stringparam suidHelper "$suidHelper" \
       --stringparam apparmor "$apparmor" \
       ${./make-system-conf.xsl} ${dbus}/share/dbus-1/system.conf \
-      > $out/system.conf
+      | xmllint --format - > $out/system.conf
     xsltproc --nonet \
       --stringparam serviceDirectories "$serviceDirectories" \
       --stringparam apparmor "$apparmor" \
       ${./make-session-conf.xsl} ${dbus}/share/dbus-1/session.conf \
-      > $out/session.conf
+      | xmllint --format - > $out/session.conf
 
     # check if files are empty or only contain space characters
     grep -q '[^[:space:]]' "$out/system.conf" || (echo "\"$out/system.conf\" was generated incorrectly and is empty, try building again." && exit 1)
